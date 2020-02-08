23dea5594b1c7cea60582dcced60f69ace60a913
Alex Tschopp <altschop@calpoly.edu>
2020-02-07 21:44:18 -0800

    Updating comment for theme to match other theme comment

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,8 +4,9 @@ import {
 } from '@material-ui/core/styles';
 import teal from '@material-ui/core/colors/teal';
 
-// Material UI's theming/styling solution 
-// (explained here: https://material-ui.com/customization/theming/ and here https://material-ui.com/styles/basics/)
+// Material UI's theming/styling solution
+//  https://material-ui.com/styles/basics/
+//  https://material-ui.com/customization/theming/
 export const theme = responsiveFontSizes(
     createTheme({
         palette: {
