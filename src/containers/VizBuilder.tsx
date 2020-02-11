5dac67a04e2e2d02ebdf57eed4b1eaf6358bab9d
Alex Tschopp <altschop@calpoly.edu>
2020-02-10 17:23:19 -0800

    Added basic navigation (#2)
    * Added basic navigation and initialized files for each page (the containers)
    
    * Update src/App.tsx
    
    Co-Authored-By: Tanner Larson <tlarso01@calpoly.edu>
    
    * Transitioned styling to use styled-components instead of hooks
    
    * Update src/theme/theme.js
    
    Co-Authored-By: Tanner Larson <tlarso01@calpoly.edu>
    
    * completing suggested change for theme.js
    
    * Updating comment for theme to match other theme comment
    
    * Forgot to update a navigation callback
    
    Co-authored-by: Tanner Larson <tlarso01@calpoly.edu>

---
+++ src/containers/VizBuilder.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function VizBuilder() {
+    return <div />
+}
+
+export default VizBuilder;
---
+++ src/nav/constants.js
@@ -0,0 +1,13 @@
+// Routes
+export const HOME = '/'
+export const EXPLORE = '/explore'
+export const VIZ_BUILDER = '/viz-builder'
+export const STORY_BUILDER = '/story-builder'
+export const MY_STUFF = '/my-stuff'
+
+// Names of routes in the navigation bar
+export const HOME_NAME = "Reach"
+export const EXPLORE_NAME = 'Explore'
+export const VIZ_BUILDER_NAME = 'VizBuilder'
+export const STORY_BUILDER_NAME = 'StoryBuilder'
+export const MY_STUFF_NAME = 'My Stuff'
