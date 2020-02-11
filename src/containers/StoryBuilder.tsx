424ef98f28954ba896bd55c350d11538f5188f61
Kellie Banzon <kelliebanzon@gmail.com>
2020-02-11 11:06:50 -0800

    Install Draft.js and add basic placeholder
     (The rich text editor is there, even if you can't see it. Can't make it super visible until later tonight, so that placeholder text is all you get for now.)

--- src/containers/StoryBuilder.tsx
+++ src/containers/StoryBuilder.tsx
@@ -1,7 +1,25 @@
+import { Container } from '@material-ui/core';
+import { Editor as TextEditor, EditorState as TextEditorState } from 'draft-js';
 import React from 'react';
 
 function StoryBuilder() {
-  return <div />;
+  const [textEditorState, setTextEditorState] = React.useState(
+    TextEditorState.createEmpty()
+  );
+
+  return (
+    <Container>
+      <h1>TODO: StoryBuilder</h1>
+      <TextEditor
+        editorState={textEditorState}
+        onChange={setTextEditorState}
+        placeholder="DRAFT.JS PLACEHOLDER TEXT EDITOR"
+      />
+    </Container>
+  );
 }
 
 export default StoryBuilder;
+
+// this code came from the draftjs getting started instructions, but i'm not sure how it's different from the functional style
+// ReactDOM.render(<MyEditor />, document.getElementById('container'));
