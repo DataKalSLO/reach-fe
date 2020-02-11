95ff63732d4ab483b22fa11c0ba4f133470d712c
mohamed aichouri <43630474+theAichouri@users.noreply.github.com>
2020-02-11 10:59:14 -0800

    Graph builder and map component added (#5)
    * hichart with TS
    
    * graph component
    
    * graph updates
    
    * graph update and maps component has been added

---
+++ src/graphs/Graph.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official';
+
+const local_data = require ('../common/assets/Local Data/dod_contracts_2018.json') ;
+
+const options : Highcharts.Options =  {
+  xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+
+  series: [{
+      type : 'bar',
+      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+  }, {
+      type : 'bar',
+      data: [216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5],
+  }]
+};
+
+
+
+
+function Graph() {
+    return (
+    <div>
+        <div>
+        <HighchartsReact
+            highcharts={Highcharts}
+            options={options}/>
+        </div>
+    </div>
+    )
+}
+
+export default Graph;
---
+++ src/maps/Map.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function Map() {
+    return (
+    <div>
+            <h1>Map goes here</h1>
+    </div>)
+}
+export default Map
