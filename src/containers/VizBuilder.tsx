07ffd4aa7d4e51497760946be212c8bf032798d7
Mohamed Aichouri <maichour@calpoly.edu>
2020-02-10 21:52:42 -0800

    hichart with TS

--- src/containers/VizBuilder.tsx
+++ src/containers/VizBuilder.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official';
+
+
+const options : Highcharts.Options = {
+   
+    title: {
+      text: 'My chart'
+    },
+    series: [
+      {
+        type: 'spline',
+        data: [1, 2, 1, 4, 3, 6]
+      }
+    ]
+  };
+
 
 function VizBuilder() {
-    return <div />
+    return (
+    <div>
+        <div>
+        <HighchartsReact
+            highcharts={Highcharts}
+            options={options}/>
+        </div>
+    </div>
+    )
 }
 
 export default VizBuilder;
