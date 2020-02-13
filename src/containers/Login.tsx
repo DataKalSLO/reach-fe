77c0bf8213e3530639794e39be7a52ef5f37343b
Shannon Marquiss <semarqui@calpoly.edu>
2020-02-12 22:50:20 -0800

    added login form for proof of concept

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import LoginForm from '../common/components/LoginForm';
 
 function Login() {
-  return <div />;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+    >
+      <LoginForm />
+    </Grid>
+  );
 }
 
 export default Login;
