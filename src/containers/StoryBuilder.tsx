083d4873107d5d37471c9bf0c2791a4f665c91cc
Kellie Banzon <kelliebanzon@gmail.com>
2020-02-14 10:53:30 -0800

    added help text

--- src/containers/StoryBuilder.tsx
+++ src/containers/StoryBuilder.tsx
@@ -6,6 +6,12 @@ function StoryBuilder() {
   return (
     <Container>
       <h1>StoryBuilder</h1>
+      <text>
+        Tell us a compelling story using data. Use the toolbar on the right to
+        add text blocks, graphs, static images, and dataset snippets to help
+        readers follow along with your findings and conclusions. Use the drag
+        handles to the left of each component if you want to reorder them.
+      </text>
       <StoryBuilderForm />
     </Container>
   );
