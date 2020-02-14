faa515611b0701c76cd3229d5d61ad07d1a4e0d6
Kellie Banzon <kelliebanzon@gmail.com>
2020-02-14 10:51:22 -0800

    reformat to match linting

--- src/nav/constants.js
+++ src/nav/constants.js
@@ -1,14 +1,14 @@
 // Routes
-export const HOME = '/'
-export const EXPLORE = '/explore'
-export const VIZ_BUILDER = '/viz-builder'
-export const STORY_BUILDER = '/story-builder'
-export const MY_STUFF = '/my-stuff'
-export const LOGIN = '/login'
+export const HOME = '/';
+export const EXPLORE = '/explore';
+export const VIZ_BUILDER = '/viz-builder';
+export const STORY_BUILDER = '/story-builder';
+export const MY_STUFF = '/my-stuff';
+export const LOGIN = '/login';
 
 // Names of routes in the navigation bar
-export const HOME_NAME = "Reach"
-export const EXPLORE_NAME = 'Explore'
-export const VIZ_BUILDER_NAME = 'VizBuilder'
-export const STORY_BUILDER_NAME = 'StoryBuilder'
-export const MY_STUFF_NAME = 'My Stuff'
+export const HOME_NAME = 'Reach';
+export const EXPLORE_NAME = 'Explore';
+export const VIZ_BUILDER_NAME = 'VizBuilder';
+export const STORY_BUILDER_NAME = 'StoryBuilder';
+export const MY_STUFF_NAME = 'My Stuff';
