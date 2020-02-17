1ea97c360e9596dca8b5608819feb0683968ad82
Mohamed Aichouri <maichour@calpoly.edu>
2020-02-17 10:43:34 -0800

    fixed variable names

--- src/graphs/Graph.tsx
+++ src/graphs/Graph.tsx
@@ -3,11 +3,11 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 type DodContract = {
-  recipient_name: string;
-  $_total_award_value: number;
-  award_type: string;
-  location: string;
-};
+  recipientName: string,
+  totalAwardValue: number,
+  awardType: string,
+  location: string
+}
 
 export const DATA = {
   dodContracts: require('../common/assets/Local Data/dod_contracts_2018.json')
@@ -15,8 +15,8 @@ export const DATA = {
 
 const data = DATA.dodContracts.map((item: DodContract) => {
   return {
-    name: item.recipient_name,
-    y: item.$_total_award_value
+    name: item.recipientName,
+    y: item.totalAwardValue
   };
 });
 const options: Highcharts.Options = {
