305e034d243455fd72390df8e1ad6f09d0f5f27e
mathmch <mathmch@gmail.com>
2020-02-18 09:37:44 -0800

    working example of google signin button, not linked to backend

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline
+} from 'react-google-login';
+import { Grid } from '@material-ui/core';
+
+const responseGoogle = (
+  response: GoogleLoginResponse | GoogleLoginResponseOffline
+) => {
+  console.log(response);
+};
 
 function Login() {
-  return <div />;
+  return (
+    <Grid
+      direction="row"
+      justify="center"
+      alignItems="center"
+      item
+      container
+      xs={12}
+    >
+      <GoogleLogin
+        clientId="771819856575-fs38pckfuc7oipvt6fr0tnugr2dlbusl.apps.googleusercontent.com"
+        buttonText="Sign in with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      ></GoogleLogin>
+    </Grid>
+  );
 }
 
 export default Login;
