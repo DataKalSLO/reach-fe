74fd186b64eefb38165ecff8efb0b08e01bca057
ecrisolo <ecrisolo@calpoly.edu>
2020-02-17 20:52:53 -0800

    made geofilter button

--- src/maps/FiltersComponent.tsx
+++ src/maps/FiltersComponent.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
-function FiltersComponent() {
-  return <div />;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'left',
+      '& > *': {
+        margin: theme.spacing(1)
+      }
+    }
+  })
+);
+
+type props = {
+  title: string,
+  paragraph: string
+}
+
+export default function BasicButtonGroup() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <ButtonGroup color="primary" aria-label="outlined primary button group">
+        <Button>Region</Button>
+        <Button>City</Button>
+        <Button>Zip Code</Button>
+      </ButtonGroup>
+    </div>
+  );
 }
