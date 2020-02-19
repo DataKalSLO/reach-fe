505726d22e0c72c326995592be98b1c9c670d6e4
Mohamed Aichouri <maichour@calpoly.edu>
2020-02-18 20:39:32 -0800

    added splitter for the home page to split between graphs and map

--- src/containers/VizBuilder.tsx
+++ src/containers/VizBuilder.tsx
@@ -3,6 +3,9 @@ import Graph from '../graphs/Graph';
 import Map from '../maps/Map';
 import { Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import SplitterLayout from 'react-splitter-layout';
+import 'react-splitter-layout/lib/index.css';
+
 function VizBuilder() {
   return (
     <Box
@@ -14,12 +17,14 @@ function VizBuilder() {
       border="1px solid red"
       alignContent="center"
     >
-      <Grid item xs={6}>
-        <Map />
-      </Grid>
-      <Grid item xs={6}>
-        <Graph />
-      </Grid>
+      <SplitterLayout  percentage = {true}  primaryMinSize = {0}  >
+        <Grid item xs={6}>
+          <Map />
+        </Grid>
+        <Grid item xs={6}>
+          <Graph />
+        </Grid>
+      </SplitterLayout>     
     </Box>
   );
 }
