c19dd8d71f5b672687c5e4421a58372489adda09
Aaron Rice <awrice@calpoly.edu>
2020-02-22 15:54:44 -0800

    Initial Structure for Login Page

---
+++ src/containers/SignIn.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Grid, TextField, Button } from '@material-ui/core';
+
+function SignIn() {
+  return (
+    <Grid container item direction="column" xs={3} spacing={3}>
+      <Grid item>
+        <TextField placeholder="Email Address" fullWidth={true} />
+      </Grid>
+      <Grid item>
+        <TextField placeholder="Password" type="password" fullWidth={true} />
+      </Grid>
+      <Grid item>
+        <Button variant="contained" disabled fullWidth={true}>
+          LOG IN
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default SignIn;
