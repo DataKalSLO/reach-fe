648f4f504e8d794ab57607737b1f301d67f5a6ab
Aaron Rice <awrice@calpoly.edu>
2020-02-25 10:13:49 -0800

    Addressed comments

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Grid, Link, Divider, Button, styled } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import SignIn from './SignIn';
-import SocialMediaSignIn from './SocialMediaSignIn';
-import { HOME } from '../nav/constants';
+import ThirdPartySignIn from './ThirdPartySignIn';
+import { HOME, CREATE_ACCOUNT } from '../nav/constants';
 
 function Login() {
   const history = useHistory();
@@ -13,16 +13,16 @@ function Login() {
     <Grid container direction="column">
       <Grid container direction="row" justify="space-between">
         <Button onClick={navigateTo(HOME)}>REACH</Button>
-        <Button onClick={navigateTo(HOME)}>CREATE ACCOUNT</Button>
+        <Button onClick={navigateTo(CREATE_ACCOUNT)}>CREATE ACCOUNT</Button>
       </Grid>
       <CenteredGrid container>
         <h1>Log into Reach</h1>
         <Grid container direction="row" justify="center" spacing={9}>
           <SignIn />
           <Divider orientation="vertical" flexItem />
-          <SocialMediaSignIn />
+          <ThirdPartySignIn />
         </Grid>
-        <Link onClick={navigateTo(HOME)}>CANT LOG IN?</Link>
+        <Link onClick={navigateTo(HOME)}>CAN&apos;T LOG IN?</Link>
       </CenteredGrid>
     </Grid>
   );
