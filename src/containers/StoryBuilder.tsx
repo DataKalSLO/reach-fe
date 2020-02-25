df413ac7da4a322e5c993be08a7d4d7b9b98755f
Kellie Banzon <kelliebanzon@gmail.com>
2020-02-25 10:30:00 -0800

    Rich Text Editor  for StoryBuilder (#19)
    Created rich text editor using DraftJS that offers some inline, heading, and list styles. Visible on the StoryBuilder page.
    
    Includes these commits:
    
    * Install Draft.js and add basic placeholder
    
     (The rich text editor is there, even if you can't see it. Can't make it super visible until later tonight, so that placeholder text is all you get for now.)
    
    * setup Formik for StoryBuilderForm, add Text Input for title
    
    * refactored rich text editor into component and added styling
    
    * connect key and button bindings for rich text formatting
    
    * added demo of JSON obj api team could receive for a basic story
    
    * added icon to save button
    
    * reformat to match linting
    
    * added text formatting options
    
    * added help text
    
    * properly link RichTextEditor w/Formik via custom Field
    
    * added select for heading styles
    
    * Revert "properly link RichTextEditor w/Formik via custom Field"
    
    This reverts commit a5dbc2c5c2ddb48b09613dfe7462dcc59c5c1732.
    
    # Conflicts:
    #	src/stories/RichTextEditor.tsx
    
    * Revert "added demo of JSON obj api team could receive for a basic story"
    
    This reverts commit dbaf2c3a3c2faf65b5ed3fd3072aa3ca27ecc45c.
    
    * Remove StoryBuilderForm
    
    * cleaned up icon imports and made DraftJS type-safe
    
    * refactored IconButton into new component
    
    * Reverted to IconButton technique
    
    Custom component was having issues with the onClick handler
    
    * Changed declarations to arrow functions
    
    Recommended by https://frontarm.com/james-k-nelson/when-to-use-arrow-functions/#dont-panic
    
    Co-authored-by: Tanner Larson <tlarso01@calpoly.edu>
    Co-authored-by: Kevin Krein <kkrein@calpoly.edu>

--- src/containers/StoryBuilder.tsx
+++ src/containers/StoryBuilder.tsx
@@ -1,7 +1,20 @@
+import { Container } from '@material-ui/core';
 import React from 'react';
+import RichTextEditor from '../stories/RichTextEditor';
 
 function StoryBuilder() {
-  return <div />;
+  return (
+    <Container>
+      <h1>StoryBuilder</h1>
+      <p>
+        Tell us a compelling story using data. Use the toolbar on the right to
+        add text blocks, graphs, static images, and dataset snippets to help
+        readers follow along with your findings and conclusions. Use the drag
+        handles to the left of each component if you want to reorder them.
+      </p>
+      <RichTextEditor />
+    </Container>
+  );
 }
 
 export default StoryBuilder;
