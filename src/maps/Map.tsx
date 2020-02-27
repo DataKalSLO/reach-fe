35873c3d5b4f9422eb60070912c1ca73946e39a8
Abby <azilvitis@gmail.com>
2020-02-27 11:56:52 -0800

    added component for data sources

--- src/maps/Map.tsx
+++ src/maps/Map.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MapView from './MapView';
 import LayersComponent from './LayersComponent';
 import { BasicButtonGroup } from './FiltersComponent';
+import SourceLabels from './SourcesComponent';
 
 function Map() {
   return (
@@ -9,6 +10,7 @@ function Map() {
       <LayersComponent />
       <MapView />
       <BasicButtonGroup />
+      <SourceLabels />
     </div>
   );
 }
---
+++ src/maps/SourcesComponent.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+
+interface Sources {
+  key: number;
+  label: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      padding: theme.spacing(0.5)
+    },
+    chip: {
+      margin: theme.spacing(0.5)
+    }
+  })
+);
+
+export default function SourceLabels() {
+  const classes = useStyles();
+  const [Sources, setSources] = React.useState<Sources[]>([
+    { key: 0, label: 'City of SLO' },
+    { key: 1, label: 'City of Santa Maria' }
+  ]);
+
+  return (
+    <Paper className={classes.root}>
+      <div> Data Sources: </div>
+      {Sources.map(data => {
+        let icon;
+        return (
+          <Chip
+            size="small"
+            key={data.key}
+            icon={icon}
+            label={data.label}
+            className={classes.chip}
+          />
+        );
+      })}
+    </Paper>
+  );
+}
