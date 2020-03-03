DataKalSLO/reach-fe

commit 22c2a17da30188b62f5e9e5bf2b10c485634a4f6

Story state (#31)
**Added**
* types definitions for a story
* state management for a story to redux
* demo of accessing story state from component

**Updated**
* lifted state from RichTextEditor


Includes the following commits: 

* Added DB objects

(cherry picked from commit 39a66a4890de1f5e2d9c0e49f2c06517d7699dec)

* In the middle of adding full functionality

(cherry picked from commit 3b6fd61a8ca45979dcbbdcd7db9ea8d29b2fcba2)

* Managing list of text editors

(cherry picked from commit 759e177346c5ba4985aba86b277a9edd5474f327)

* lift state from RichTextEditor to StoryForm

(cherry picked from commit 4e32d6823438685ae99f089edfbc100b1597eb14)

* Finished logic for managing story in state

(cherry picked from commit 3147d23182ca523c858c532a9d9595f1d4e18c5c)

* refactor story state from useReducer to Redux

(cherry picked from commit 90f60dee72db672918914d4876c11559c273be8a)

* add demo of accessing story state from Redux

* update placeholder values

* address feedback from types.ts

* address feedback from actions.ts

* address feedback from reducer.ts

* clean-up linting warnings

* remove utility functions

* cleanup StoryType

Co-authored-by: Kevin Krein <kkrein@calpoly.edu>

src/redux/story/actions.ts
@@ -1,0 +1,43 @@
+import { EditorState } from 'draft-js';
+import uuidv4 from 'uuid/v4';
+import {
+  CREATE_EMPTY_TEXT_BLOCK,
+  SWAP_BLOCKS,
+  UPDATE_TEXT_BLOCK,
+  CreateEmptyTextBlockAction,
+  UpdateTextBlockAction,
+  SwapBlocksAction
+} from './types';
+
+export function createTextBlock(): CreateEmptyTextBlockAction {
+  return {
+    type: CREATE_EMPTY_TEXT_BLOCK,
+    payload: {
+      block: {
+        id: uuidv4(),
+        editorState: EditorState.createEmpty(),
+        type: 'Text' // TODO: loosly typed attribute, planned fix rolling out soon
+      }
+    }
+  };
+}
+
+export function updateTextBlock(
+  index: number,
+  editorState: EditorState
+): UpdateTextBlockAction {
+  return {
+    type: UPDATE_TEXT_BLOCK,
+    payload: { index: index, editorState: editorState }
+  };
+}
+
+export function swapBlocks(
+  oldIndex: number,
+  newIndex: number
+): SwapBlocksAction {
+  return {
+    type: SWAP_BLOCKS,
+    payload: { oldIndex: oldIndex, newIndex: newIndex }
+  };
+}

src/redux/story/reducer.ts
@@ -1,0 +1,70 @@
+import { EditorState } from 'draft-js';
+import { arrayMove } from 'react-sortable-hoc';
+import uuidv4 from 'uuid/v4';
+import { StoryBlock } from '../../stories/StoryTypes';
+import {
+  CREATE_EMPTY_TEXT_BLOCK,
+  StoryActionType,
+  SWAP_BLOCKS,
+  UpdateBlockType,
+  UPDATE_TEXT_BLOCK
+} from './types';
+
+const initialTextBlock = {
+  id: uuidv4(),
+  editorState: EditorState.createEmpty(),
+  type: 'Text' // TODO: loosly typed attribute, planned fix rolling out soon
+};
+
+const initialState = {
+  id: uuidv4(),
+  userID: 'USER-ID', // TODO: replace placeholder value
+  title: '',
+  description: '',
+  storyBlocks: [initialTextBlock] as Array<StoryBlock>
+};
+
+// follows immutability update patterns
+// (https://redux.js.org/recipes/structuring-reducers/immutable-update-patterns/)
+function updateObjectInArray(
+  storyBlocks: Array<StoryBlock>,
+  action: UpdateBlockType
+) {
+  return storyBlocks.map((item, i) => {
+    // updates desired storyblock
+    if (action.payload.index === i) {
+      return {
+        ...item,
+        ...action.payload
+      };
+    }
+    // ignores rest of storyblocks
+    return item;
+  });
+}
+
+export function storyReducer(state = initialState, action: StoryActionType) {
+  switch (action.type) {
+    case UPDATE_TEXT_BLOCK:
+      return {
+        ...state,
+        storyBlocks: updateObjectInArray(state.storyBlocks, action)
+      };
+    case CREATE_EMPTY_TEXT_BLOCK:
+      return {
+        ...state,
+        storyBlocks: state.storyBlocks.concat(action.payload.block)
+      };
+    case SWAP_BLOCKS:
+      return {
+        ...state,
+        storyBlocks: arrayMove(
+          state.storyBlocks,
+          action.payload.oldIndex,
+          action.payload.newIndex
+        )
+      };
+    default:
+      return state;
+  }
+}

src/containers/StoryBuilder.tsx
@@ -1,6 +1,6 @@
 import { Container } from '@material-ui/core';
 import React from 'react';
-import RichTextEditor from '../stories/RichTextEditor';
+import StoryForm from '../stories/StoryForm';
 
 function StoryBuilder() {
   return (
@@ -12,7 +12,7 @@ function StoryBuilder() {
         readers follow along with your findings and conclusions. Use the drag
         handles to the left of each component if you want to reorder them.
       </p>
-      <RichTextEditor />
+      <StoryForm />
     </Container>
   );
 }

src/redux/index.ts
@@ -5,12 +5,15 @@ import { User } from './login/types';
 import { userReducer } from './login/reducer';
 import { Sample } from './sample/types';
 import { sampleReducer } from './sample/reducer';
+import { Story } from '../stories/StoryTypes';
+import { storyReducer } from './story/reducer';
 import { VizState } from './vizbuilder/types';
 import { vizReducer } from './vizbuilder/reducer';
 
 function createRootReducer(history: History) {
   return combineReducers({
     sample: sampleReducer,
+    story: storyReducer,
     user: userReducer,
     vizbuilder: vizReducer,
     router: connectRouter(history)
@@ -19,6 +22,7 @@ function createRootReducer(history: History) {
 
 export interface RootState {
   sample: Sample;
+  story: Story;
   user: User;
   vizbuilder: VizState;
   router: RouterState;

src/redux/story/selectors.ts
@@ -1,0 +1,3 @@
+import { RootState } from '../index';
+
+export const getStory = (store: RootState) => store.story;

src/redux/story/types.ts
@@ -1,0 +1,33 @@
+import { EditorState } from 'draft-js';
+import { TextBlock } from '../../stories/StoryTypes';
+
+export const CREATE_EMPTY_TEXT_BLOCK = 'CREATE_EMPTY_TEXT_BLOCK';
+export const UPDATE_TEXT_BLOCK = 'UPDATE_TEXT_BLOCK';
+export const SWAP_BLOCKS = 'SWAP_BLOCKS';
+
+export interface CreateEmptyTextBlockAction {
+  type: typeof CREATE_EMPTY_TEXT_BLOCK;
+  payload: { block: TextBlock };
+}
+
+export interface UpdateTextBlockAction {
+  type: typeof UPDATE_TEXT_BLOCK;
+  payload: { index: number; editorState: EditorState };
+}
+
+export interface SwapBlocksAction {
+  type: typeof SWAP_BLOCKS;
+  payload: { oldIndex: number; newIndex: number };
+}
+
+// use in updateObjectInArray function (reducer.ts) when updating a single object.
+// interfaces of this type must include:
+//  - index
+//  - <data-to-change>
+export type UpdateBlockType = UpdateTextBlockAction;
+
+// used by reducer function (reducer.ts)
+export type StoryActionType =
+  | CreateEmptyTextBlockAction
+  | UpdateTextBlockAction
+  | SwapBlocksAction;

src/stories/RichTextEditor.tsx
@@ -15,11 +15,16 @@ import {
   StrikethroughS
 } from '@material-ui/icons';
 import { Editor, EditorState, RichUtils } from 'draft-js';
-import React, { useState } from 'react';
 import { DraftJSBlockType, DraftJSInlineType } from './DraftJSCommands';
+import React from 'react';
 
-export default function RichTextEditor() {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+interface Props {
+  editorState: EditorState;
+  setEditorState: (s: EditorState) => void;
+}
+
+export default function RichTextEditor(props: Props) {
+  const { editorState, setEditorState } = props; // this is NOT a hook, the state is being managed in StoryForm
 
   // enable key binding shortcuts (e.g. CTRL+B for bold)
   const handleKeyCommand = (command: string, editorState: EditorState) => {

src/stories/StoryForm.tsx
@@ -1,0 +1,46 @@
+// DEMO: accessing story state from Redux
+//
+// PLEASE DON'T CODE REVIEW THIS FILE
+// It will be completely rewritten in a future PR.
+
+import React from 'react';
+import SaveIcon from '@material-ui/icons/Save';
+import { Button } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import { getStory } from '../redux/story/selectors';
+import { updateTextBlock } from '../redux/story/actions';
+import RichTextEditor from './RichTextEditor';
+import { TextBlock } from './StoryTypes';
+import { EditorState } from 'draft-js';
+
+export default function StoryForm() {
+  const dispatch = useDispatch();
+  const story = useSelector(getStory);
+
+  function saveStory() {
+    alert(JSON.stringify(story.storyBlocks, null, 2));
+    console.log(JSON.stringify(story));
+    return story;
+  }
+
+  return (
+    <div>
+      <h1>{story.title}</h1>
+      <h4>{story.description}</h4>
+      <RichTextEditor
+        editorState={(story.storyBlocks[0] as TextBlock).editorState}
+        setEditorState={(editorState: EditorState) =>
+          dispatch(updateTextBlock(0, editorState))
+        }
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={saveStory}
+        startIcon={<SaveIcon />}
+      >
+        Save Story
+      </Button>
+    </div>
+  );
+}

commit d2c861a70b0e04d3d58353d2d6dbb0c9fbeb0342

update package.json and import for uuid (#32)
fixes missing imports that broke the build in #31

src/redux/story/actions.ts
@@ -1,5 +1,5 @@
 import { EditorState } from 'draft-js';
-import uuidv4 from 'uuid/v4';
+import { uuid } from 'uuidv4';
 import {
   CREATE_EMPTY_TEXT_BLOCK,
   SWAP_BLOCKS,
@@ -14,7 +14,7 @@ export function createTextBlock(): CreateEmptyTextBlockAction {
     type: CREATE_EMPTY_TEXT_BLOCK,
     payload: {
       block: {
-        id: uuidv4(),
+        id: uuid(),
         editorState: EditorState.createEmpty(),
         type: 'Text' // TODO: loosly typed attribute, planned fix rolling out soon
       }

src/redux/story/reducer.ts
@@ -1,6 +1,6 @@
 import { EditorState } from 'draft-js';
 import { arrayMove } from 'react-sortable-hoc';
-import uuidv4 from 'uuid/v4';
+import { uuid } from 'uuidv4';
 import { StoryBlock } from '../../stories/StoryTypes';
 import {
   CREATE_EMPTY_TEXT_BLOCK,
@@ -11,13 +11,13 @@ import {
 } from './types';
 
 const initialTextBlock = {
-  id: uuidv4(),
+  id: uuid(),
   editorState: EditorState.createEmpty(),
   type: 'Text' // TODO: loosly typed attribute, planned fix rolling out soon
 };
 
 const initialState = {
-  id: uuidv4(),
+  id: uuid(),
   userID: 'USER-ID', // TODO: replace placeholder value
   title: '',
   description: '',