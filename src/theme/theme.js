89a204d6021f9322118ad723c47dab0f69579294
Kevin Krein <kevin.krein98@gmail.com>
2020-03-03 23:53:55 -0800

    Accidentially removed line in random file

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,6 +3,7 @@ import {
     responsiveFontSizes
 } from '@material-ui/core/styles';
 import teal from '@material-ui/core/colors/teal';
+
 // Material UI's theming/styling solution
 //  https://material-ui.com/styles/basics/
 //  https://material-ui.com/customization/theming/
