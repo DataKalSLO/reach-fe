2edfe4413a0a8d0a4675e9379da2215937a3d837
mathmch <mathmch@gmail.com>
2020-03-10 11:15:58 -0700

    default google sign in button with backend connection

--- src/containers/GoogleAuth.tsx
+++ src/containers/GoogleAuth.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { GoogleLoginButton } from 'ts-react-google-login-component';
-import { Button, styled } from '@material-ui/core';
-import GoogleIcon from '../icons/GoogleIcon';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { LoginData, RegisterData } from '../redux/login/types';
 import { loginUser, register } from '../redux/login/actions';
 import { HOME } from '../nav/constants';
+import GoogleBtn from '../icons/btn_google_signin_dark_normal_web.png';
 
 const GoogleAuth = (props: { isRegistration: boolean }) => {
   const history = useHistory();
@@ -18,46 +17,40 @@ const GoogleAuth = (props: { isRegistration: boolean }) => {
       '771819856575-fs38pckfuc7oipvt6fr0tnugr2dlbusl.apps.googleusercontent.com'
   };
 
-  const responseGoogle = (googleUser: gapi.auth2.GoogleUser): void => {
-    if (props.isRegistration) {
-      dispatch(
-        register({
-          email: googleUser.getBasicProfile().getEmail(),
-          password: googleUser.getId(),
-          name: googleUser.getBasicProfile().getName(),
-          role: 'BaseUser'
-        } as RegisterData)
-      );
-    } else {
-      dispatch(
-        loginUser({
-          email: googleUser.getBasicProfile().getEmail(),
-          password: googleUser.getId()
-        } as LoginData)
-      );
-    }
-    history.push(HOME);
-  };
-
-  const errorHandler = (error: string): void => {
-    console.error(error);
-  };
-
-  const StyledButton = styled(Button)({
-    width: '270px',
-    height: '50px'
-  });
+  const responseGoogle = useCallback(
+    (googleUser: gapi.auth2.GoogleUser): void => {
+      if (props.isRegistration) {
+        dispatch(
+          register({
+            email: googleUser.getBasicProfile().getEmail(),
+            password: googleUser.getId(),
+            name: googleUser.getBasicProfile().getName(),
+            role: 'BaseUser'
+          } as RegisterData)
+        );
+      } else {
+        dispatch(
+          loginUser({
+            email: googleUser.getBasicProfile().getEmail(),
+            password: googleUser.getId()
+          } as LoginData)
+        );
+      }
+      history.push(HOME);
+    },
+    [dispatch, history, props.isRegistration]
+  );
 
   return (
     <GoogleLoginButton
       classNames="custom_class center-block"
       responseHandler={responseGoogle}
       clientConfig={clientConfig}
-      failureHandler={errorHandler}
+      failureHandler={error => {
+        console.error(error);
+      }}
     >
-      <StyledButton endIcon={<GoogleIcon />} fullWidth variant="outlined">
-        Continue with Google
-      </StyledButton>
+      <img src={GoogleBtn} alt="Sign in with Google" />
     </GoogleLoginButton>
   );
 };
