dc414519a21a6e28b3e84887b0d9f43c9f968739
Levi Thompson <leviwthompson@gmail.com>
2020-03-09 20:14:58 -0700

    add redux-persist package with 'user' whitelisted

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,14 +4,27 @@ import { routerMiddleware } from 'connected-react-router';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createRootReducer from './index';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'] // Top level store branches to persist
+};
 
 export const history = createBrowserHistory();
 
 const middleware = [thunk, routerMiddleware(history)];
 
-const store = createStore(
-  createRootReducer(history),
+const persistedReducer = persistReducer(
+  persistConfig,
+  createRootReducer(history)
+);
+
+export const store = createStore(
+  persistedReducer,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
-export default store;
+export const persistor = persistStore(store);
