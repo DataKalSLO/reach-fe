1b75fb230985c93e24b4d4407ae7a8cfc43e1afa
mathmch <mathmch@gmail.com>
2020-03-10 09:29:57 -0700

    everything together and working

--- src/containers/ThirdPartySignIn.tsx
+++ src/containers/ThirdPartySignIn.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import { Button, styled } from '@material-ui/core';
+import { styled } from '@material-ui/core';
 import BoxCenter from '../common/components/BoxCenter';
-import GoogleIcon from '../icons/GoogleIcon';
+import GoogleAuth from './GoogleAuth';
 import FacebookLogin, { FacebookLoginButtonStyle } from './FacebookLogin';
 
 // A component to handle signing in using existing Social Media Accounts
 function ThirdPartySignIn() {
   return (
     <ThirdPartyBox>
-      <GoogleSignIn />
+      <GoogleAuth isRegistration={false} />
       <FacebookLogin style={FacebookLoginButtonStyle.LoginWith} />
     </ThirdPartyBox>
   );
 }
-
-const GoogleSignIn = () => {
-  return (
-    <StyledButton endIcon={<GoogleIcon />} fullWidth variant="outlined">
-      Continue with Google
-    </StyledButton>
-  );
-};
 
 const paddingDefault = '40px';
 
@@ -29,11 +21,6 @@ const ThirdPartyBox = styled(BoxCenter)({
   paddingRight: paddingDefault,
   width: '270px',
   height: '115px'
-});
-
-const StyledButton = styled(Button)({
-  width: '270px',
-  height: '50px'
 });
 
 export default ThirdPartySignIn;
