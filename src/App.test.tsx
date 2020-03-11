fa2e9f479c24203b3200e900754c04b67dd5c8bf
Alex Tschopp <altschop@calpoly.edu>
2020-03-10 17:55:45 -0700

    Revert "new-testing-stub (#53)"
    This reverts commit 96c464556106fc79cc0e801f53707f664b654e1c.

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { HOME } from './nav/constants';
-
-// Jest docs: https://jestjs.io/docs/en/getting-started
-
-describe('Jest testing stub', () => {
-  it('Super simple test', () => {
-    expect(HOME).toBe('/');
-  });
-});
