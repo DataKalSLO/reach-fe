4a7006e8c933fcf36f017e428c0c62ba741de465
mmaramba <mpmaramb@calpoly.edu>
2020-03-10 21:20:41 -0700

    Michaels GraphShareButton

--- src/graphs/forms/constants.ts
+++ src/graphs/forms/constants.ts
@@ -5,3 +5,4 @@ export const DELETE_LABEL = 'Delete';
 export const SAVE_LABEL = 'Save';
 export const EDIT_LABEL = 'Edit';
 export const DUPLICATE_LABEL = 'Duplicate';
+export const SHARE_LABEL = 'Share';
