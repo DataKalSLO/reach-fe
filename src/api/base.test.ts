6e606733193ad8cdd53d4a42274625df8432ee85
Zach Mitchell <zdmitche@calpoly.edu>
2020-03-16 13:59:25 -0700

    constructGetParameters: add tests
    This tests three different scenerios for testing the
    `constructGetParameters` method. The first is a single key value pair,
    the second two, and the third none. This method is supposed to construct
    get parameters using the "? and &" notation based on key value pairs and
    return a string to be appended to a url.

---
+++ src/api/base.test.ts
@@ -0,0 +1,18 @@
+import { constructGetParameters } from "./base";
+
+describe("constructGetParameters util function", () => {
+  it("one value", () => {
+    expect(constructGetParameters([{ test1: "value1" }]) === "?test=value1");
+  });
+
+  it("multiple values", () => {
+    expect(
+      constructGetParameters([{ test1: "value1" }, { test2: "value2" }]) ===
+        "?test=value1&test2=value2"
+    );
+  });
+
+  it("empty values", () => {
+    expect(constructGetParameters([]) === "");
+  });
+});
