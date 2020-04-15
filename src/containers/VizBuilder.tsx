f106ce2e390af51e4e304cd88edd84b29464446b
Alex Tschopp <altschop@calpoly.edu>
2020-04-15 17:50:23 -0500

    changed styling method to match repo standard

--- src/containers/VizBuilder.tsx
+++ src/containers/VizBuilder.tsx
@@ -1,38 +1,68 @@
 import React, { Fragment } from 'react';
 import { Grid, Container } from '@material-ui/core';
-import { useVizBuilderStyles } from './VizBuilderStyles';
 import Map from '../maps/Map';
 import Graph from '../graphs/Graph';
 import SplitterLayout from 'react-splitter-layout';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { styled } from '@material-ui/core/styles';
 
 import 'react-splitter-layout/lib/index.css';
 
 function VizBuilder() {
-  const classes = useVizBuilderStyles();
   return (
     <Fragment>
-      <Container className={classes.root} maxWidth={'xl'}>
+      <StyledContainer maxWidth={'xl'}>
         <SplitterLayout>
-          <Grid item className={classes.grid} xs={12}>
-            <NavigateBeforeIcon
-              className={classes.leftArrow}
-              fontSize={'large'}
-            />
+          <StyledGrid item xs={12}>
+            <LeftArrow fontSize={'large'} />
             <Map />
-          </Grid>
-          <Grid item className={classes.grid} xs={12}>
-            <NavigateNextIcon
-              className={classes.rightArrow}
-              fontSize={'large'}
-            />
+          </StyledGrid>
+          <StyledGrid item xs={12}>
+            <RightArrow fontSize={'large'} />
             <Graph />
-          </Grid>
+          </StyledGrid>
         </SplitterLayout>
-      </Container>
+      </StyledContainer>
     </Fragment>
   );
 }
+
+const StyledContainer = styled(Container)({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  height: '90vh',
+  padding: '0px',
+  position: 'relative',
+  overflow: 'hidden',
+  '& .splitter-layout .layout-pane': {
+    overflow: 'hidden'
+  }
+});
+
+const StyledGrid = styled(Grid)({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100%',
+  overflow: 'hidden'
+});
+
+const LeftArrow = styled(NavigateBeforeIcon)({
+  right: '0',
+  top: '50%',
+  position: 'absolute',
+  zIndex: 500
+});
+
+const RightArrow = styled(NavigateNextIcon)({
+  left: '0',
+  top: '50%',
+  position: 'absolute',
+  zIndex: 500
+});
 
 export default VizBuilder;
