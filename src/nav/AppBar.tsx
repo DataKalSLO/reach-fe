03bb532d9a429f6817cc0588277f4432c9fc4ba8
azilvitis <26912285+azilvitis@users.noreply.github.com>
2020-04-16 15:36:28 -0700

    Map builder (#76)
    Adding the entire map feature (big time apologies for the massive PR and we are extremely thankful to all of you who spent time and energy to look through it)

--- src/nav/AppBar.tsx
+++ src/nav/AppBar.tsx
@@ -109,7 +109,7 @@ function AppBar() {
           />
         </Grid>
       </Grid>
-      <Grid container justify="flex-end" xs={2}>
+      <Grid item container justify="flex-end" xs={2}>
         <Grid item>
           {user.email ? (
             <React.Fragment>
