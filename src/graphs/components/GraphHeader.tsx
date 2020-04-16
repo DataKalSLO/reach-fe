355cf1aa52177b6c2e60b9c98977029fac1dfa3b
RD77 <dominguez7777@gmail.com>
2020-04-16 03:30:07 -0700

    Graph prebuilt initiatives (#73)
    * add prebuilt graph components
    
    * fix store graph get method
    
    * remove unused graph component
    
    * make prebuilt graphs accessible
    
    * use common button components
    
    * organize imports
    
    * add better explanations
    
    * fix styles
    
    * fix button styles

---
+++ src/graphs/components/GraphHeader.tsx
@@ -0,0 +1,80 @@
+import { Delete, Edit, FileCopy, Save, Share } from '@material-ui/icons';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Button } from '../../common/components/Button';
+import {
+  deleteGraphAction,
+  duplicateGraphAction
+} from '../../redux/graphs/actions';
+import {
+  DEFAULT_KEY,
+  DELETE_LABEL,
+  DUPLICATE_LABEL,
+  EDIT_LABEL,
+  SAVE_LABEL
+} from './constants';
+import { StyledHeaderGrid } from './styles';
+import { GraphHeaderProps } from './types';
+
+/*
+ * Contains the buttons rendered on the header of a graph.
+ * Note: defaultFlag is only used to prevent the user from
+ *       deleting and duplicating the default graph. This
+ *       will get removed when we connect to the backend.
+ */
+
+function GraphHeader({ graph }: GraphHeaderProps) {
+  const dispatch = useDispatch();
+  // TODO: change the way default graphs are handled
+  const defaultFlag = graph.id === DEFAULT_KEY;
+
+  return (
+    <StyledHeaderGrid container>
+      <Button
+        label={DELETE_LABEL}
+        color="default"
+        variant="text"
+        startIcon={<Delete color="error" />}
+        onClick={() => {
+          if (!defaultFlag) {
+            dispatch(deleteGraphAction(graph.id));
+          }
+        }}
+      />
+      <Button
+        label={SAVE_LABEL}
+        variant="text"
+        color="default"
+        startIcon={<Save />}
+        onClick={() => alert('Not implemented')}
+      />
+      <Button
+        label={EDIT_LABEL}
+        variant="text"
+        color="default"
+        startIcon={<Edit />}
+        onClick={() => alert('Not implemented')}
+      />
+      <Button
+        label={DUPLICATE_LABEL}
+        variant="text"
+        color="default"
+        startIcon={<FileCopy />}
+        onClick={() => {
+          if (!defaultFlag) {
+            dispatch(duplicateGraphAction(graph.options));
+          }
+        }}
+      />
+      <Button
+        label="Share"
+        variant="text"
+        color="default"
+        startIcon={<Share />}
+        onClick={() => alert('Not implemented')}
+      />
+    </StyledHeaderGrid>
+  );
+}
+
+export default GraphHeader;
