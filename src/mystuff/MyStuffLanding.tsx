317483aeca6f0342bee8c618b8bc63ab13089e7b
louiseibuna <louisemibuna@gmail.com>
2020-05-02 18:14:30 -0700

    header styling

--- src/mystuff/MyStuffLanding.tsx
+++ src/mystuff/MyStuffLanding.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, styled, Box } from '@material-ui/core';
+
+export const StyledMyStuffHeader = styled(Typography)({
+  textAlign: 'left',
+  fontWeight: 'bold',
+  marginTop: '40px',
+  marginLeft: '40px',
+  marginBottom: '40px'
+});
+
+const StyledBox = styled(Box)({
+  textAlign: 'center'
+});
 
 function MyStuffLanding() {
-  return <Typography variant="h4">My Folders</Typography>;
+  return (
+    <StyledBox>
+      <StyledMyStuffHeader variant="h4">My Folders</StyledMyStuffHeader>
+    </StyledBox>
+  );
 }
 
 export default MyStuffLanding;
