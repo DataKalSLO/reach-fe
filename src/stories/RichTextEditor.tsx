11aff452f29d90e043eb7c82f46cc0f26f421505
Tanner Larson <tannerlarson07@gmail.com>
2020-05-05 21:35:45 -0700

    fix spacing issue between RichTextEditor and delete button

--- src/stories/RichTextEditor.tsx
+++ src/stories/RichTextEditor.tsx
@@ -48,7 +48,7 @@ const paddingDefault = '10px';
 
 //TODO: @tan refactor into StoryBlockWrapper
 export const StyledBox = styled(Box)({
-  width: '100%',
+  // width: '100%',
   minHeight: '100px',
   minWidth: '510px',
   border: '1px solid #cbcbcb', // same border style is used in EditorToolbar
