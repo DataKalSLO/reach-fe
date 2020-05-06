adf37e3f07c4d76bd42276155c21466d64627ac3
Tanner Larson <tannerlarson07@gmail.com>
2020-05-05 23:20:42 -0700

    tweaked hide width constants

--- src/stories/EditorToolbar.tsx
+++ src/stories/EditorToolbar.tsx
@@ -158,8 +158,8 @@ const EditorToolbar = (props: Props) => {
 
 // Screenwidth when a group of items to become hidden in the collapsible menu
 const DEFAULT_HIDE_WIDTH = 0;
-const LIST_STYLE_HIDE_WIDTH = 800;
-const LINK_STYLE_HIDE_WIDTH = 900;
+const LIST_STYLE_HIDE_WIDTH = 860;
+const LINK_STYLE_HIDE_WIDTH = 920;
 
 const borderStyle = '1px solid #cbcbcb';
 const paddingDefault = '10px';
