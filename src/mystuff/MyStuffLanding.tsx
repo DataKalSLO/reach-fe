9bb84f75fa2645b8f4bcdd0c55269cf7f6b5bb8b
louiseibuna <louisemibuna@gmail.com>
2020-05-05 18:17:18 -0700

    forgot to delete import

--- src/mystuff/MyStuffLanding.tsx
+++ src/mystuff/MyStuffLanding.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Typography, styled, Box } from '@material-ui/core';
-import CollectionGrid from './CollectionGrid';
 
 export const StyledMyStuffHeader = styled(Typography)({
   textAlign: 'left',
@@ -18,7 +17,6 @@ function MyStuffLanding() {
   return (
     <StyledBox>
       <StyledMyStuffHeader variant="h4">My Folders</StyledMyStuffHeader>
-      <CollectionGrid />
     </StyledBox>
   );
 }
