24f6556092eebea4b9c6aad8d8fe4db0f1b69c68
louiseibuna <louisemibuna@gmail.com>
2020-05-05 18:50:14 -0700

    my stuff link deleted from AppBar

--- src/nav/AppBar.tsx
+++ src/nav/AppBar.tsx
@@ -108,12 +108,6 @@ function AppBar() {
             navigateToRoute={navigateTo(STORY_BUILDER)}
           />
         </Grid>
-        <Grid item key={MY_STUFF_NAME}>
-          <MenuButton
-            name={MY_STUFF_NAME}
-            navigateToRoute={navigateTo(MY_STUFF)}
-          />
-        </Grid>
       </Grid>
       <Grid item container justify="flex-end" xs={2}>
         <Grid item>
