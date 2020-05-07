DataKalSLO/reach-fe

commit bcf5d35bff4966d6510ee13959a79b923afc72e3

TextBlock collapsible toolbar (#111)
Summary of changes:
Menu bar items in the TextBlock toolbar automatically move into the hidden menu as window shrinks. The items are accessible through the more icon ("three-dots"). @Daniel-Heyde will continue making UI tweaks with a future PR. 

Fixes: 
Fixes [REAC-137](http://platinum.cscaws.com:8080/browse/REAC-137)

Includes these commits: 
* show more button on window resize

* refactor: create functions for each styled group

* add collapsable menu and collapsable items

* refactor Collapsables in seperate file

* add ability to toggle hidden menu items

* add minWidth to EditorToolbar

* splitIndex updates dynamically based on breakwidths!

* refactor: finish grouping linkStyle code into a single component

* fix: isCollapsed func

* fix bug that required you to click twice on toolbar icons before they would register

* improve variable names and comments

* improve readibility

* fix spelling error

* improve variable names and comments

* cleanup unused variables/imports

src/stories/EditorToolbar.tsx
@@ -19,6 +19,7 @@ import {
 } from '@material-ui/icons';
 import { EditorState, RichUtils } from 'draft-js';
 import React, { useState } from 'react';
+import { CollapsibleItem, CollapsibleMenu } from './CollapsibleToolbar';
 import { DraftJSBlockType, DraftJSInlineType } from './DraftJSCommands';
 import { HyperlinkPlugin } from './HyperlinkPlugin';
 
@@ -40,21 +41,7 @@ const EditorToolbar = (props: Props) => {
     setURLValue
   );
 
-  // connect icon buttons in the toolbar to the state updates for inline styles
-  const onClickInlineStyle = (buttonName: DraftJSInlineType) => {
-    props.setEditorState(
-      RichUtils.toggleInlineStyle(props.editorState, buttonName)
-    );
-  };
-
-  // connect icon buttons and heading select in the toolbar to the state updates for block styles
-  const onClickBlockType = (buttonName: DraftJSBlockType) => {
-    props.setEditorState(
-      RichUtils.toggleBlockType(props.editorState, buttonName)
-    );
-  };
-
-  let urlInputUI;
+  let urlInputUI: {} | undefined;
   if (showURLInput) {
     urlInputUI = (
       <URLInputBox>
@@ -80,72 +67,99 @@ const EditorToolbar = (props: Props) => {
       </URLInputBox>
     );
   }
+  // connect icon buttons in the toolbar to the state updates for inline styles
+  const onClickInlineStyle = (buttonName: DraftJSInlineType) => {
+    props.setEditorState(
+      RichUtils.toggleInlineStyle(props.editorState, buttonName)
+    );
+  };
+
+  // connect icon buttons and heading select in the toolbar to the state updates for block styles
+  const onClickBlockType = (buttonName: DraftJSBlockType) => {
+    props.setEditorState(
+      RichUtils.toggleBlockType(props.editorState, buttonName)
+    );
+  };
 
   return (
     <Toolbar>
-      {/* text styles */}
-      <FormatButtonGroup>
-        <StyledTextField
-          id="heading-style-select"
-          select
-          label="Heading Style"
-          variant="outlined"
-          margin="dense"
-          value={RichUtils.getCurrentBlockType(props.editorState)}
-          onChange={value =>
-            onClickBlockType(value.target.value as DraftJSBlockType)
-          }
-          defaultValue={'unstyled'}
-        >
-          <MenuItem value={'unstyled'}>Normal text</MenuItem>
-          <MenuItem value={'header-one'}>Heading 1</MenuItem>
-          <MenuItem value={'header-two'}>Heading 2</MenuItem>
-          <MenuItem value={'header-three'}>Heading 3</MenuItem>
-          <MenuItem value={'header-four'}>Heading 4</MenuItem>
-          <MenuItem value={'header-five'}>Heading 5</MenuItem>
-          <MenuItem value={'header-six'}>Heading 6</MenuItem>
-        </StyledTextField>
+      <CollapsibleMenu>
+        {/* text styles */}
+        <CollapsibleItem hideWidth={DEFAULT_HIDE_WIDTH}>
+          <FormatButtonGroup>
+            <StyledTextField
+              id="heading-style-select"
+              select
+              label="Heading Style"
+              variant="outlined"
+              margin="dense"
+              value={RichUtils.getCurrentBlockType(props.editorState)}
+              onChange={value =>
+                onClickBlockType(value.target.value as DraftJSBlockType)
+              }
+              defaultValue={'unstyled'}
+            >
+              <MenuItem value={'unstyled'}>Normal text</MenuItem>
+              <MenuItem value={'header-one'}>Heading 1</MenuItem>
+              <MenuItem value={'header-two'}>Heading 2</MenuItem>
+              <MenuItem value={'header-three'}>Heading 3</MenuItem>
+              <MenuItem value={'header-four'}>Heading 4</MenuItem>
+              <MenuItem value={'header-five'}>Heading 5</MenuItem>
+              <MenuItem value={'header-six'}>Heading 6</MenuItem>
+            </StyledTextField>
 
-        {/* TODO: refactor these to use the common IconButton */}
-        <IconButton onClick={() => onClickInlineStyle('BOLD')}>
-          <FormatBold />
-        </IconButton>
-        <IconButton onClick={() => onClickInlineStyle('ITALIC')}>
-          <FormatItalic />
-        </IconButton>
-        <IconButton onClick={() => onClickInlineStyle('UNDERLINE')}>
-          <FormatUnderlined />
-        </IconButton>
-        <IconButton onClick={() => onClickInlineStyle('STRIKETHROUGH')}>
-          <StrikethroughS />
-        </IconButton>
-        <IconButton onClick={() => onClickInlineStyle('CODE')}>
-          <Code />
-        </IconButton>
-      </FormatButtonGroup>
+            {/* TODO: refactor these to use the common IconButton */}
+            <IconButton onClick={() => onClickInlineStyle('BOLD')}>
+              <FormatBold />
+            </IconButton>
+            <IconButton onClick={() => onClickInlineStyle('ITALIC')}>
+              <FormatItalic />
+            </IconButton>
+            <IconButton onClick={() => onClickInlineStyle('UNDERLINE')}>
+              <FormatUnderlined />
+            </IconButton>
+            <IconButton onClick={() => onClickInlineStyle('STRIKETHROUGH')}>
+              <StrikethroughS />
+            </IconButton>
+            <IconButton onClick={() => onClickInlineStyle('CODE')}>
+              <Code />
+            </IconButton>
+          </FormatButtonGroup>
+        </CollapsibleItem>
 
-      {/* list styles */}
-      <FormatButtonGroup>
-        <IconButton onClick={() => onClickBlockType('unordered-list-item')}>
-          <FormatListBulleted />
-        </IconButton>
-        <IconButton onClick={() => onClickBlockType('ordered-list-item')}>
-          <FormatListNumbered />
-        </IconButton>
-      </FormatButtonGroup>
+        {/* list styles */}
+        <CollapsibleItem hideWidth={LIST_STYLE_HIDE_WIDTH}>
+          <FormatButtonGroup>
+            <IconButton onClick={() => onClickBlockType('unordered-list-item')}>
+              <FormatListBulleted />
+            </IconButton>
+            <IconButton onClick={() => onClickBlockType('ordered-list-item')}>
+              <FormatListNumbered />
+            </IconButton>
+          </FormatButtonGroup>
+        </CollapsibleItem>
 
-      <FormatButtonGroup>
-        <IconButton onClick={hyperlinkPlugin.promptForLink}>
-          <Link />
-        </IconButton>
-        <IconButton onClick={hyperlinkPlugin.removeLink}>
-          <LinkOff />
-        </IconButton>
-        {urlInputUI}
-      </FormatButtonGroup>
+        {/* link styles */}
+        <CollapsibleItem hideWidth={LINK_STYLE_HIDE_WIDTH}>
+          <FormatButtonGroup>
+            <IconButton onClick={hyperlinkPlugin.promptForLink}>
+              <Link />
+            </IconButton>
+            <IconButton onClick={hyperlinkPlugin.removeLink}>
+              <LinkOff />
+            </IconButton>
+            {urlInputUI}
+          </FormatButtonGroup>
+        </CollapsibleItem>
+      </CollapsibleMenu>
     </Toolbar>
   );
 };
+
+// Screenwidth when a group of items to become hidden in the collapsible menu
+const DEFAULT_HIDE_WIDTH = 0;
+const LIST_STYLE_HIDE_WIDTH = 800;
+const LINK_STYLE_HIDE_WIDTH = 900;
 
 const borderStyle = '1px solid #cbcbcb';
 const paddingDefault = '10px';
@@ -153,11 +167,14 @@ const paddingDefault = '10px';
 const Toolbar = styled(Box)({
   display: 'flex',
   flexDirection: 'row',
+  width: '100%',
   borderBottom: borderStyle,
   marginBottom: '10px',
-  padding: '5px 0px 5px 0px'
+  padding: '5px 0px 5px 0px',
+  minWidth: '440px'
 });
 
+// Adds vertical bar on the rhs of the group
 const FormatButtonGroup = styled(Box)({
   display: 'flex',
   alignItems: 'center',

src/stories/RichTextEditor.tsx
@@ -1,6 +1,6 @@
 import { Box, styled } from '@material-ui/core';
 import { Editor, EditorState, RichUtils } from 'draft-js';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { KEY_COMMAND_FAILURE, KEY_COMMAND_SUCCESS } from './DraftJSCommands';
 import EditorToolbar from './EditorToolbar';
 import { HyperlinkDecorator } from './HyperlinkDecorator';
@@ -48,6 +48,7 @@ const paddingDefault = '10px';
 
 //TODO: @tan refactor into StoryBlockWrapper
 export const StyledBox = styled(Box)({
+  width: '100%',
   minHeight: '100px',
   minWidth: '510px',
   border: '1px solid #cbcbcb', // same border style is used in EditorToolbar

src/stories/CollapsibleToolbar.tsx
@@ -1,0 +1,91 @@
+import { Box, styled } from '@material-ui/core';
+import { MoreHorizRounded } from '@material-ui/icons';
+import { useWindowWidth } from '@react-hook/window-size/throttled';
+import React, { useState, useEffect } from 'react';
+import { IconButton } from '../common/components/IconButton';
+
+interface CollapsibleItemProps {
+  hideWidth: number; // determines when an item will be moved to the hidden menu
+  children: React.ReactNode;
+}
+
+const CollapsibleItem = (props: CollapsibleItemProps) => (
+  <StyledBox>{props.children}</StyledBox>
+);
+
+interface CollapsibleMenuProps {
+  children: React.ReactNode[];
+}
+
+const CollapsibleMenu = (props: CollapsibleMenuProps) => {
+  const hideWidths: number[] = props.children.map(
+    (child: any) => child.props.hideWidth
+  );
+  const maxNumVisibleItems = props.children.length;
+  const [numVisibleItems, setNumVisibleItems] = useState(maxNumVisibleItems);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(
+    numVisibleItems < maxNumVisibleItems
+  );
+  const windowWidth = useWindowWidth();
+
+  useEffect(() => {
+    // check if more items should be visible
+    if (hideWidths[numVisibleItems] < windowWidth) {
+      setNumVisibleItems(numVisibleItems + 1);
+      setIsMenuOpen(false);
+    }
+
+    // check if less items should be visible
+    if (hideWidths[numVisibleItems - 1] > windowWidth) {
+      setNumVisibleItems(numVisibleItems - 1);
+      setIsMenuOpen(false);
+    }
+
+    setIsCollapsed(numVisibleItems < maxNumVisibleItems);
+  }, [hideWidths, maxNumVisibleItems, numVisibleItems, windowWidth]);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  function getVisibleItems() {
+    return props.children.slice(0, numVisibleItems);
+  }
+
+  function getHiddenItems() {
+    if (isMenuOpen) {
+      return props.children.slice(numVisibleItems, maxNumVisibleItems);
+    } else return null;
+  }
+
+  function showMoreIcon() {
+    if (isCollapsed) {
+      return (
+        <IconButton
+          aria-label={'more editor options'}
+          onClick={toggleMenu}
+          icon={<MoreHorizRounded />}
+        />
+      );
+    } else return null;
+  }
+
+  return (
+    <div>
+      <StyledBox>
+        {getVisibleItems()}
+        {showMoreIcon()}
+      </StyledBox>
+      {/* TODO: @daniel add floating menu here */}
+      <StyledBox>{getHiddenItems()}</StyledBox>
+    </div>
+  );
+};
+
+export { CollapsibleMenu, CollapsibleItem };
+
+const StyledBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center'
+});

commit a8f367cbf5d8ccab967f94556e3123004aaa5b77

Fix spacing between TextBlock and delete button (#134)
This fixes a small UI bug introduced in #111 where the delete button was too close to the TextBlock.


Includes these commits: 
* remove unused imports

* fix spacing issue between RichTextEditor and delete button

* tweaked hide width constants

* removed minWidths

* Revert "tweaked hide width constants"

This reverts commit adf37e3f07c4d76bd42276155c21466d64627ac3.

src/stories/EditorToolbar.tsx
@@ -189,8 +189,7 @@ const Toolbar = styled(Box)({
   width: '100%',
   borderBottom: borderStyle,
   marginBottom: '10px',
-  padding: '5px 0px 5px 0px',
-  minWidth: '440px'
+  padding: '5px 0px 5px 0px'
 });
 
 // Adds vertical bar on the rhs of the group

src/stories/RichTextEditor.tsx
@@ -1,6 +1,6 @@
 import { Box, styled } from '@material-ui/core';
 import { Editor, EditorState, RichUtils } from 'draft-js';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { KEY_COMMAND_FAILURE, KEY_COMMAND_SUCCESS } from './DraftJSCommands';
 import EditorToolbar from './EditorToolbar';
 import { HyperlinkDecorator } from './HyperlinkDecorator';
@@ -48,9 +48,7 @@ const paddingDefault = '10px';
 
 //TODO: @tan refactor into StoryBlockWrapper
 export const StyledBox = styled(Box)({
-  width: '100%',
   minHeight: '100px',
-  minWidth: '510px',
   border: '1px solid #cbcbcb', // same border style is used in EditorToolbar
   borderRadius: '4px',
   paddingLeft: paddingDefault,