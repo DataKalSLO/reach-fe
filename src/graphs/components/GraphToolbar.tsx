35ab0504c0b623a8b71c3c2cb1ab0bacb7c06118
Roberto Dominguez <Dominguez7777@gmail.com>
2020-05-10 01:19:44 -0700

    change header to toolbar

---
+++ src/graphs/components/GraphToolbar.tsx
@@ -0,0 +1,82 @@
+import { Box, styled, Toolbar } from '@material-ui/core';
+import { Delete, Edit, FileCopy, Save, Share } from '@material-ui/icons';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Button, Props as ButtonProps } from '../../common/components/Button';
+import {
+  deleteGraphAction,
+  duplicateGraphAction
+} from '../../redux/graphs/actions';
+import {
+  DELETE_LABEL,
+  DUPLICATE_LABEL,
+  EDIT_LABEL,
+  SAVE_LABEL,
+  SHARE_LABEL
+} from './constants';
+import { GraphHeaderProps } from './types';
+
+/*
+ * Contains the buttons rendered on the graph toolbar.
+ */
+
+const ToolbarButton = (props: ButtonProps) => {
+  return <Button variant="text" color="default" {...props} />;
+};
+
+function GraphToolbar({ graph }: GraphHeaderProps) {
+  const dispatch = useDispatch();
+
+  // TODO: Collapse toolbar when width is too small
+  return (
+    <GraphStyledToolbar>
+      <GraphButtonGroup>
+        <ToolbarButton
+          label={DELETE_LABEL}
+          startIcon={<Delete color="error" />}
+          onClick={() => {
+            dispatch(deleteGraphAction(graph.id));
+          }}
+        />
+        <ToolbarButton
+          label={SAVE_LABEL}
+          startIcon={<Save />}
+          onClick={() => alert('Not implemented')}
+        />
+        <ToolbarButton
+          label={EDIT_LABEL}
+          variant="text"
+          color="default"
+          startIcon={<Edit />}
+          onClick={() => alert('Not implemented')}
+        />
+        <ToolbarButton
+          label={DUPLICATE_LABEL}
+          startIcon={<FileCopy />}
+          onClick={() => {
+            dispatch(duplicateGraphAction(graph.options));
+          }}
+        />
+        <ToolbarButton
+          label={SHARE_LABEL}
+          startIcon={<Share />}
+          onClick={() => alert('Not implemented')}
+        />
+      </GraphButtonGroup>
+    </GraphStyledToolbar>
+  );
+}
+
+export default GraphToolbar;
+
+/*
+ * Styles
+ */
+const GraphStyledToolbar = styled(Toolbar)({
+  overflow: 'scroll',
+  margin: '0px 10px 0px 10px'
+});
+
+const GraphButtonGroup = styled(Box)({
+  display: 'flex'
+});
--- src/graphs/components/styles.ts
+++ src/graphs/components/styles.ts
@@ -1,43 +1,10 @@
-import {
-  BottomNavigationAction,
-  Divider,
-  Paper,
-  Toolbar,
-  Box
-} from '@material-ui/core';
+import { BottomNavigationAction } from '@material-ui/core';
 import {
   createStyles,
   makeStyles,
   styled,
   Theme
 } from '@material-ui/core/styles';
-
-/*
- * Graph Header Styles
- */
-
-export const StyledBox = styled(Box)({
-  display: 'inline-block',
-  width: '100%'
-});
-
-export const StyledToolbar = styled(Toolbar)({
-  overflow: 'scroll',
-  margin: '0px 10px 0px 10px'
-});
-
-/*
- * Graph Component Styles
- */
-
-export const StyledGraphPaper = styled(Paper)({
-  borderWidth: '4px'
-});
-
-export const StyledGraphDivider = styled(Divider)({
-  height: '3px',
-  marginBottom: '10px'
-});
 
 /*
  * Options Bar Styles
