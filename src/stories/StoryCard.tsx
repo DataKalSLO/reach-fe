f854fc335ac018411fada89d88ca3c37b2954ab9
Kellie Banzon <kelliebanzon@gmail.com>
2020-05-11 15:01:56 -0700

    remove actions (moved to a separate pr)

--- src/stories/StoryCard.tsx
+++ src/stories/StoryCard.tsx
@@ -1,18 +1,13 @@
 import {
   Box,
   Card,
-  CardActions,
   CardContent,
   CardMedia,
   Typography
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Delete, Edit, Note } from '@material-ui/icons';
 import React from 'react';
-import { Button } from '../common/components/Button';
-import { IconButton } from '../common/components/IconButton';
 import { Story } from '../redux/story/types';
-import { theme } from '../theme/theme';
 
 const PLACEHOLDER_AUTHOR_NAME = 'Bill Writer';
 const PLACEHOLDER_DATE = '1/1/20';
@@ -32,9 +27,6 @@ const useStyles = makeStyles({
     margin: '0 2px',
     transform: 'scale(0.8)'
   },
-  actions: {
-    float: 'right'
-  },
   media: {
     height: 0,
     paddingTop: '56.25%' // 16:9
@@ -52,30 +44,6 @@ export default function StoryCard(props: StoryCardProps): JSX.Element {
 
   return (
     <Card className={classes.root} variant="outlined">
-      <CardActions className={classes.actions}>
-        <Button
-          onClick={() => alert('delete!')}
-          edge="end"
-          aria-label="Delete block"
-          style={{ 'background-color': theme.palette.primary.main }}
-          endIcon={<Note />}
-          label="Draft"
-        />
-        <IconButton
-          onClick={() => alert('delete!')}
-          edge="end"
-          aria-label="Delete block"
-          style={{ color: theme.palette.primary.main }}
-          icon={<Edit />}
-        />
-        <IconButton
-          onClick={() => alert('delete!')}
-          edge="end"
-          aria-label="Delete block"
-          style={{ color: theme.palette.error.main }}
-          icon={<Delete />}
-        />
-      </CardActions>
       <CardContent>
         <Typography className={classes.title}>{props.story.title}</Typography>
         <Typography variant="h5" component="h2">
