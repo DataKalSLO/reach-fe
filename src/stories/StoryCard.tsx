f5e5582c52d07540795e2764b4f7c48db15d815e
Kellie Banzon <kelliebanzon@gmail.com>
2020-05-11 17:50:28 -0700

    styled author and date

--- src/stories/StoryCard.tsx
+++ src/stories/StoryCard.tsx
@@ -1,15 +1,20 @@
 import {
-  Box,
   Card,
   CardContent,
   CardMedia,
-  Typography
+  Grid,
+  Typography,
+  CardHeader
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { Story } from '../redux/story/types';
+import { AccountCircle } from '@material-ui/icons';
 
-const PLACEHOLDER_AUTHOR_NAME = 'Bill Writer';
+const PLACEHOLDER_TITLE = 'High School Graduation Rates on the Central Coast';
+const PLACEHOLDER_DESCRIPTION =
+  "High school graduation rates are a critical metric of the effectiveness of our local high schools, as well as our students' college preparedness. This study examines our schools by comparing them to one another as well as state and national metrics.";
+const PLACEHOLDER_AUTHOR = 'Bill Writer';
 const PLACEHOLDER_DATE = '1/1/20';
 const PLACEHOLDER_IMAGE_URL =
   'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.stack.imgur.com%2FLuPIV.png&f=1&nofb=1';
@@ -31,9 +36,6 @@ const useStyles = makeStyles({
     height: 0,
     paddingTop: '56.25%' // 16:9
   },
-  title: {
-    fontSize: '2em'
-  },
   pos: {
     marginBottom: 12
   }
@@ -42,30 +44,62 @@ const useStyles = makeStyles({
 export default function StoryCard(props: StoryCardProps): JSX.Element {
   const classes = useStyles();
 
+  // TODO: this should take a user object. name and picture will be refactored to come from user
+  const Author = (props: { name: string; picture: JSX.Element }) => {
+    return (
+      <Grid container alignItems="center" spacing={1}>
+        <Grid item>{props.picture}</Grid>
+        <Grid item>
+          <Typography variant="subtitle1" color="textSecondary">
+            {props.name}
+          </Typography>
+        </Grid>
+      </Grid>
+    );
+  };
+
+  const AuthorDateMetadata = (props: {
+    name: string;
+    picture: JSX.Element;
+    date: string;
+  }) => {
+    return (
+      <Grid container alignItems="center" justify="space-between" spacing={0}>
+        <Grid item>
+          <Author name={props.name} picture={props.picture} />
+        </Grid>
+        <Grid item>
+          <Typography variant="subtitle1" color="textSecondary">
+            {props.date}
+          </Typography>
+        </Grid>
+      </Grid>
+    );
+  };
+
   return (
     <Card className={classes.root} variant="outlined">
+      <CardHeader title={PLACEHOLDER_TITLE} />
       <CardContent>
-        <Typography className={classes.title}>{props.story.title}</Typography>
-        <Typography variant="h5" component="h2">
-          {props.story.description}
-        </Typography>
-        <Box
-          display="flex"
-          flex-direction="row"
-          justify-content="space-between"
-        >
-          <Typography variant="h5" component="h2">
-            {PLACEHOLDER_AUTHOR_NAME}
-          </Typography>
-          <Typography variant="h5" component="h2">
-            {PLACEHOLDER_DATE}
-          </Typography>
-        </Box>
-        <CardMedia
-          className={classes.media}
-          image={PLACEHOLDER_IMAGE_URL}
-          title={props.story.title}
-        />
+        <Grid container direction="column" justify="flex-start" spacing={3}>
+          <Grid item>
+            <Typography variant="subtitle2">
+              {PLACEHOLDER_DESCRIPTION}
+            </Typography>
+          </Grid>
+
+          <AuthorDateMetadata
+            name={PLACEHOLDER_AUTHOR}
+            picture={<AccountCircle />}
+            date={PLACEHOLDER_DATE}
+          />
+
+          <CardMedia
+            className={classes.media}
+            image={PLACEHOLDER_IMAGE_URL}
+            title={props.story.title}
+          />
+        </Grid>
       </CardContent>
     </Card>
   );
