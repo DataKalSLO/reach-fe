0a7e8191b7c8526075b5be1e8b2f8288c10ae7fe
Roberto Dominguez <Dominguez7777@gmail.com>
2020-05-16 10:15:46 -0700

    add series builder helper methods

--- src/graphs/builder/utilities.ts
+++ src/graphs/builder/utilities.ts
@@ -1,4 +1,42 @@
 import { isUndefined } from 'util';
+import {
+  GraphDataXValue,
+  GraphDataYValue,
+  GraphData,
+  PrimarySeriesTypes,
+  primarySeriesTypesEnum,
+  SecondarySeriesTypes,
+  secondarySeriesTypesEnum
+} from './types';
+
+/*
+ * Pairs the values in one data array with another to create
+ * an array of tuples.
+ */
+export function zipData(
+  data1: GraphDataXValue[],
+  data2: GraphDataYValue[]
+): GraphData {
+  return data1.map((value, index) => [value, data2[index]]);
+}
+
+/*
+ * PrimarySeriesType Type Guard
+ */
+export function isPrimarySeriesType(
+  seriesType: string
+): seriesType is PrimarySeriesTypes {
+  return seriesType in primarySeriesTypesEnum;
+}
+
+/*
+ * SecondarySeriesType Type Guard
+ */
+export function isSecondarySeriesType(
+  seriesType: string
+): seriesType is SecondarySeriesTypes {
+  return seriesType in secondarySeriesTypesEnum;
+}
 
 export function getEmptyStringIfUndefined(value: string | undefined): string {
   return !isUndefined(value) ? value : '';
