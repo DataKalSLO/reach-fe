cd36851795a51846e904b982522af28c50b6ecfe
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-05-25 12:46:20 -0700

    Moved status changing functions into `operationHandlers.ts`

--- src/api/stories/operationHandlers.ts
+++ src/api/stories/operationHandlers.ts
@@ -5,11 +5,11 @@ import {
   getAllStories
 } from './operations';
 import { UNAUTHORIZED_OPERATION_ERROR } from '../authenticatedApi/constants';
-import { Story } from '../../redux/story/types';
+import { Story, PublicationStatus } from '../../redux/story/types';
 
 /* These functions are meant to be wrappers for accessing the Story API.
- * They are meant to be used when you don't want to handle when an API
- * fails.
+ * They alert the user is in operation fails. Then, a boolean is returned
+ * indicating if the operation was successful.
  */
 const STORY_CREATION_SUCCESS_MESSAGE = 'Story created!';
 const STORY_CREATION_FAILURE_MESSAGE =
@@ -21,14 +21,48 @@ const STORY_RETRIEVAL_SUCCESS_MESSAGE = 'Story retrieved!';
 const STORY_RETRIEVAL_FAILURE_MESSAGE =
   'An Error occurred while retrieving a Story.';
 
-export async function saveStoryAndHandleResponse(
+const STORY_CHANGE_STATUS_SUCCESS_MESSAGE = 'Status of story was changed!.';
+const STORY_CHANGE_STATUS_FAILURE_MESSAGE =
+  'An Error occurred while changing the status of the story. Status has not been changed.';
+
+/*
+ * Application Specific Operations
+ */
+
+export async function submitStoryForReviewAndHandleResponse(
   story: Story
+): Promise<boolean> {
+  story.publicationStatus = PublicationStatus.REVIEW;
+  return await saveStoryAndHandleResponse(
+    story,
+    STORY_CHANGE_STATUS_SUCCESS_MESSAGE,
+    STORY_CHANGE_STATUS_FAILURE_MESSAGE
+  );
+}
+
+export async function submitStoryForPublishingAndHandleResponse(story: Story) {
+  story.publicationStatus = PublicationStatus.PUBLISHED;
+  return await saveStoryAndHandleResponse(
+    story,
+    STORY_CHANGE_STATUS_SUCCESS_MESSAGE,
+    STORY_CHANGE_STATUS_FAILURE_MESSAGE
+  );
+}
+
+/*
+ * CRUD Operations
+ */
+
+export async function saveStoryAndHandleResponse(
+  story: Story,
+  successMessage: string = STORY_CREATION_SUCCESS_MESSAGE,
+  failureMessage: string = STORY_CREATION_FAILURE_MESSAGE
 ): Promise<boolean> {
   return await handleApiOperation(
     story,
     saveOrUpdateExistingStory,
-    STORY_CREATION_SUCCESS_MESSAGE,
-    STORY_CREATION_FAILURE_MESSAGE
+    successMessage,
+    failureMessage
   )
     .then(res => true)
     .catch(e => false);
