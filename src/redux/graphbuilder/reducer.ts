bba73fe0d0b045dedd9f473b22ddcb38b2d42d25
Roberto Dominguez <Dominguez7777@gmail.com>
2020-05-26 12:56:16 -0700

    add graph builder reducer

---
+++ src/redux/graphbuilder/reducer.ts
@@ -0,0 +1,79 @@
+import { isDefinedElse } from '../../graphs/forms/utilities';
+import { DELETE_GRAPH, DUPLICATE_GRAPH } from '../graphs/constants';
+import {
+  CREATE_GRAPH,
+  GET_ALL_USER_GRAPHS,
+  GET_DEFAULT_GRAPHS_FOR_CATEGORY,
+  GET_GRAPH,
+  SAVE_GRAPH,
+  UPDATE_GRAPH,
+  UPDATE_LOCAL_GRAPH
+} from './constants';
+import { GraphActionTypes, GraphBuilderState } from './types';
+import {
+  duplicateGraph,
+  insertIfDefinedAtIndex,
+  replaceGraph
+} from './utilities';
+
+const initialState: GraphBuilderState = {
+  graphs: [],
+  isCreating: false
+};
+
+export function graphBuilderReducer(
+  state = initialState,
+  action: GraphActionTypes
+): GraphBuilderState {
+  switch (action.type) {
+    case SAVE_GRAPH:
+      return {
+        ...state,
+        graphs: insertIfDefinedAtIndex(state.graphs, 0, true, action.payload)
+      };
+    case UPDATE_GRAPH:
+      return {
+        ...state,
+        graphs: replaceGraph(state.graphs, action.payload)
+      };
+    case DELETE_GRAPH:
+      return {
+        ...state,
+        graphs: state.graphs.filter(
+          graph => graph.graphMetaData.graphId !== action.payload?.graphId
+        )
+      };
+    case GET_GRAPH:
+      return {
+        ...state,
+        graphs: insertIfDefinedAtIndex(state.graphs, 0, false, action.payload)
+      };
+    case GET_ALL_USER_GRAPHS:
+      return {
+        ...state,
+        graphs: isDefinedElse(action.payload, state.graphs)
+      };
+    case GET_DEFAULT_GRAPHS_FOR_CATEGORY:
+      return {
+        ...state,
+        graphs: isDefinedElse(action.payload, state.graphs)
+      };
+    case UPDATE_LOCAL_GRAPH:
+      return {
+        ...state,
+        graphs: replaceGraph(state.graphs, action.payload)
+      };
+    case DUPLICATE_GRAPH:
+      return {
+        ...state,
+        graphs: duplicateGraph(state.graphs, action.payload)
+      };
+    case CREATE_GRAPH:
+      return {
+        ...state,
+        isCreating: !state.isCreating
+      };
+    default:
+      return state;
+  }
+}
