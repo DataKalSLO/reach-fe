f007133293e1ddea557923ef1fc022a5caed62ac
Zach Mitchell <zdmitche@calpoly.edu>
2020-05-26 10:44:47 -0700

    Lint: run Prettier

--- src/api/base.test.ts
+++ src/api/base.test.ts
@@ -1,18 +1,18 @@
-import { constructGetParameters } from "./base";
+import { constructGetParameters } from './base';
 
-describe("constructGetParameters util function", () => {
-  it("one value", () => {
-    expect(constructGetParameters([{ test1: "value1" }]) === "?test=value1");
+describe('constructGetParameters util function', () => {
+  it('one value', () => {
+    expect(constructGetParameters([{ test1: 'value1' }]) === '?test=value1');
   });
 
-  it("multiple values", () => {
+  it('multiple values', () => {
     expect(
-      constructGetParameters([{ test1: "value1" }, { test2: "value2" }]) ===
-        "?test=value1&test2=value2"
+      constructGetParameters([{ test1: 'value1' }, { test2: 'value2' }]) ===
+        '?test=value1&test2=value2'
     );
   });
 
-  it("empty values", () => {
-    expect(constructGetParameters([]) === "");
+  it('empty values', () => {
+    expect(constructGetParameters([]) === '');
   });
 });
