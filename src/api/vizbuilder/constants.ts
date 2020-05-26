4900dcc431a0a8af0b08bf6042ef4cdc9b316e9a
RD77 <dominguez7777@gmail.com>
2020-05-26 10:29:58 -0700

    Vizbuilder api operations (#181)
    * add/modify vizbuilder types
    
    * add graph data type
    
    * add date type checker
    
    * add vizbuilder api operations

---
+++ src/api/vizbuilder/constants.ts
@@ -0,0 +1,7 @@
+export const METADATA_RETRIEVAL_SUCCESS_MESSAGE = 'Metadata retrieved!';
+export const METADATA_RETRIEVAL_FAILURE_MESSAGE =
+  'An Error occurred while retrieving the Metadata.';
+
+export const COLUMNS_RETRIEVAL_SUCCESS_MESSAGE = 'Columns retrieved!';
+export const COLUMNS_RETRIEVAL_FAILURE_MESSAGE =
+  'An Error occurred while retrieving the Columns.';
