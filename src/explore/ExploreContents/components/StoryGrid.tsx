530514cf166f237888e70f8fd4683899f6f53a6b
Nathan Tu <nathanhuytu@gmail.com>
2020-05-27 22:03:47 -0700

    Refactor to use Gallery component and deleted old code

--- src/explore/ExploreContents/components/StoryGrid.tsx
+++ src/explore/ExploreContents/components/StoryGrid.tsx
@@ -1,28 +1,10 @@
 import React, { useEffect } from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Grid, { GridSpacing } from '@material-ui/core/Grid';
 import StoryCard from '../../../preview-cards/story-card/StoryCard';
 import { getAllStoriesAndHandleResponse } from '../../../api/stories/operationHandlers';
 import { Story } from '../../../redux/story/types';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1
-    },
-    paper: {
-      height: 140,
-      width: 100
-    },
-    control: {
-      padding: theme.spacing(2)
-    }
-  })
-);
+import Gallery from '../../../common/components/Gallery';
 
 export default function StoryGrid() {
-  const [spacing] = React.useState<GridSpacing>(2);
-  const classes = useStyles();
   const [stories, setStories] = React.useState([] as Story[]);
 
   useEffect(() => {
@@ -40,15 +22,8 @@ export default function StoryGrid() {
   };
 
   const DisplayStories = () => {
-    return (
-      <Grid container className={classes.root} spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justify="center" spacing={spacing}>
-            {makeStories()}
-          </Grid>
-        </Grid>
-      </Grid>
-    );
+    const storyCards = makeStories();
+    return <Gallery>{storyCards}</Gallery>;
   };
   return <DisplayStories />;
 }
