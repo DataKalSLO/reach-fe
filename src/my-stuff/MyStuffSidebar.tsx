d1fcfce102882e314e75a931fb40e6d4e3c80aad
Tanner Larson <tannerlarson07@gmail.com>
2020-05-29 11:48:38 -0700

    clean up MyStuffSidebar

--- src/my-stuff/MyStuffSidebar.tsx
+++ src/my-stuff/MyStuffSidebar.tsx
@@ -12,7 +12,6 @@ import {
   Drawer,
   getPersistentDrawerOptions,
   List,
-  ListItemButton,
   ListItemTab
 } from '../reach-ui/core';
 
@@ -23,77 +22,39 @@ const drawerOptions: PersistentDrawerProps = {
   drawerId: 'MyStuffSidebar'
 };
 
-const drawerOptions2: PersistentDrawerProps = {
-  defaultTab: 'Maps',
-  drawerId: 'Right'
-};
-
 export default function MyStuffSidebar() {
   const history = useHistory();
   const navigateTo = (route: string) => history.push(route);
-
   const options = getPersistentDrawerOptions(drawerOptions);
-  const options2 = getPersistentDrawerOptions(drawerOptions2);
 
   return (
-    <>
-      <Drawer width={SIDEBAR_WIDTH} isCollapsible={true}>
-        <List>
-          <ListItemTab
-            title="All Items"
-            icon={<ViewModule />}
-            onClick={() => navigateTo(MY_STUFF)}
-            {...options}
-          />
-          <ListItemTab
-            title="Charts"
-            icon={<InsertChart />}
-            onClick={() => navigateTo(MY_STUFF_CHARTS)}
-            {...options}
-          />
-          <ListItemTab
-            title="Maps"
-            icon={<Map />}
-            onClick={() => navigateTo(MY_STUFF_MAPS)}
-            {...options}
-          />
-          <ListItemTab
-            title="Stories"
-            icon={<LibraryBooks />}
-            onClick={() => navigateTo(MY_STUFF_STORIES)}
-            {...options}
-          />
-        </List>
-      </Drawer>
-      <Drawer width={SIDEBAR_WIDTH} isCollapsible={true} anchor={'right'}>
-        <List>
-          <ListItemTab
-            title="All Items"
-            icon={<ViewModule />}
-            onClick={() => navigateTo(MY_STUFF)}
-            {...options2}
-          />
-          <ListItemTab
-            title="Charts"
-            icon={<InsertChart />}
-            onClick={() => navigateTo(MY_STUFF_CHARTS)}
-            {...options2}
-          />
-          <ListItemTab
-            title="Maps"
-            icon={<Map />}
-            onClick={() => navigateTo(MY_STUFF_MAPS)}
-            {...options2}
-          />
-          <ListItemTab
-            title="Stories"
-            icon={<LibraryBooks />}
-            onClick={() => navigateTo(MY_STUFF_STORIES)}
-            {...options2}
-          />
-          <ListItemButton text="yeee" icon={<LibraryBooks />} />
-        </List>
-      </Drawer>
-    </>
+    <Drawer width={SIDEBAR_WIDTH} isCollapsible={true}>
+      <List>
+        <ListItemTab
+          title="All Items"
+          icon={<ViewModule />}
+          onClick={() => navigateTo(MY_STUFF)}
+          {...options}
+        />
+        <ListItemTab
+          title="Charts"
+          icon={<InsertChart />}
+          onClick={() => navigateTo(MY_STUFF_CHARTS)}
+          {...options}
+        />
+        <ListItemTab
+          title="Maps"
+          icon={<Map />}
+          onClick={() => navigateTo(MY_STUFF_MAPS)}
+          {...options}
+        />
+        <ListItemTab
+          title="Stories"
+          icon={<LibraryBooks />}
+          onClick={() => navigateTo(MY_STUFF_STORIES)}
+          {...options}
+        />
+      </List>
+    </Drawer>
   );
 }
