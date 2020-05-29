e6cb3cb20f9b240ad5dd179f56c64f3932e87e91
Mohamed Aichouri <maichour@calpoly.edu>
2020-05-28 22:53:17 -0700

    snackbar alert when saving a graph

--- src/graphs/components/GraphToolbar.tsx
+++ src/graphs/components/GraphToolbar.tsx
@@ -1,42 +1,55 @@
-import { Toolbar } from '@material-ui/core';
+import { makeStyles, Snackbar, Theme, Toolbar } from '@material-ui/core';
 import { Delete, Edit, FileCopy, Save, Share } from '@material-ui/icons';
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Button, { Props as ButtonProps } from '../../common/components/Button';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
-import {
-  deleteGraphAction,
-  duplicateGraphAction
-} from '../../redux/graphs/actions';
-import {
-  DELETE_LABEL,
-  DUPLICATE_LABEL,
-  EDIT_LABEL,
-  SAVE_LABEL,
-  SHARE_LABEL,
-  SHOW_LABEL,
-  HIDE_LABEL
-} from './constants';
-import { GraphHeaderProps } from './types';
-import { GraphMetaDataApiPayload } from '../../redux/graphs/types';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { saveGraph } from '../../api/graphs/operations';
-import {
-  updateGraph,
-  deleteGraph,
-  duplicateGraph
-} from '../../redux/graphbuilder/actions';
+import Button, { Props as ButtonProps } from '../../common/components/Button';
+import { deleteGraph, duplicateGraph, updateGraph } from '../../redux/graphbuilder/actions';
+import { GraphMetaDataApiPayload } from '../../redux/graphs/types';
 import { getUser } from '../../redux/login/selectors';
+import { DELETE_LABEL, DUPLICATE_LABEL, EDIT_LABEL, HIDE_LABEL, SAVE_LABEL, SHARE_LABEL, SHOW_LABEL } from './constants';
+import { GraphHeaderProps } from './types';
 
 /*
  * Contains the buttons rendered on the graph toolbar.
  */
 
 const ToolbarButton = (props: ButtonProps) => {
-  return <Button variant="text" color="default" {...props} />;
+  return <Button variant="text" color="info" {...props} />;
 };
 
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2)
+    }
+  }
+}));
+
 function GraphToolbar(props: GraphHeaderProps) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   const { graph, index, isHidden, toggleEdit, toggleHide } = props;
   const dispatch = useDispatch();
   const userInfo = useSelector(getUser);
@@ -64,12 +77,23 @@ function GraphToolbar(props: GraphHeaderProps) {
       <ToolbarButton
         label={SAVE_LABEL}
         startIcon={<Save />}
-        onClick={() =>
+        onClick={() => {
           graph.graphMetaData.graphId === ''
             ? dispatch(saveGraph(newGraph))
-            : dispatch(updateGraph(newGraph))
-        }
+            : dispatch(updateGraph(newGraph));
+          handleClick();
+        }}
       />
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity="success">
+          Saving was a success!
+        </Alert>
+      </Snackbar>
       <ToolbarButton
         label={DUPLICATE_LABEL}
         startIcon={<FileCopy />}
