2177e0bf0b537b17c4a895125c5a6d987c0f3c63
Roberto Dominguez <Dominguez7777@gmail.com>
2020-05-29 17:38:52 -0700

    connect bottom drawer buttons

--- src/graphs/container/OptionsBar.tsx
+++ src/graphs/container/OptionsBar.tsx
@@ -1,10 +1,13 @@
 import { BottomNavigationAction, Drawer } from '@material-ui/core';
 import { ArrowUpward, Close } from '@material-ui/icons';
+import BarChartIcon from '@material-ui/icons/BarChart';
 import clsx from 'clsx';
 import React, { Fragment } from 'react';
+import { useDispatch } from 'react-redux';
 import { IconButton } from '../../reach-ui/core';
+import { getAllUserGraphs } from '../../redux/graphbuilder/actions';
 import OptionsButtons from '../components/OptionsButtons';
-import { INITIATIVE_DESC } from './constants';
+import { INITIATIVE_DESC, MY_GRAPHS_LABEL } from './constants';
 import {
   StyledBottomNav,
   StyledPopover,
@@ -20,6 +23,7 @@ import {
  */
 
 function OptionsBar() {
+  const dispatch = useDispatch();
   const classes = useOptionsStyles();
   const [open, setOpen] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
@@ -75,6 +79,12 @@ function OptionsBar() {
             icon={<Close fontSize="large" color="error" />}
           />
           <OptionsButtons />
+          <BottomNavigationAction
+            color="default"
+            onClick={() => dispatch(getAllUserGraphs())}
+            label={MY_GRAPHS_LABEL}
+            icon={<BarChartIcon fontSize="large" />}
+          />
         </StyledBottomNav>
       </Drawer>
     </Fragment>
