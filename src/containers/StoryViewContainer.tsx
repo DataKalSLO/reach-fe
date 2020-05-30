b70049f31c635aa99106e8050de2e7b281e64095
Kevin Krein <kkrein@calpoly.edu>
2020-05-29 22:34:26 -0700

    Minor tweaks

--- src/containers/StoryViewContainer.tsx
+++ src/containers/StoryViewContainer.tsx
@@ -1,9 +1,9 @@
+import { Box, styled } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { getStoryWithStoryID } from '../api/stories/operations';
 import { Story } from '../redux/story/types';
 import StoryView from '../stories/StoryView';
-import { styled, Box } from '@material-ui/core';
 import { theme } from '../theme/theme';
 
 function StoryViewContainer() {
@@ -28,14 +28,15 @@ function StoryViewContainer() {
       });
   });
 
-  // Put story preview component somewhere in here
-  return story ? (
-    <ContentBox>
-      <StoryView story={story} />
-    </ContentBox>
-  ) : (
-    <React.Fragment />
-  );
+  if (story) {
+    return (
+      <ContentBox>
+        <StoryView story={story} />
+      </ContentBox>
+    );
+  } else {
+    return <React.Fragment />;
+  }
 }
 
 const ContentBox = styled(Box)({
