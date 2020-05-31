c268bf549a72079313bc387f1960f4aa86a5195f
Nathan Tu <nathanhuytu@gmail.com>
2020-05-31 13:26:11 -0700

    Implement common box component

--- src/containers/Explore.tsx
+++ src/containers/Explore.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import ExploreGrid from '../explore/ExploreGrid';
+import PaddedBox from '../common/components/ContentBox';
 import { Typography, Box, styled } from '@material-ui/core';
-import { theme } from '../theme/theme';
 
-const StyledBox = styled(Box)({
-  paddingTop: theme.spacing(3),
-  paddingBottom: theme.spacing(3),
-  paddingLeft: theme.spacing(12),
-  paddingRight: theme.spacing(12)
-});
 function Explore() {
   return (
-    <StyledBox>
+    <PaddedBox>
       <Typography variant="h3" component="h1">
         Published Stories
       </Typography>
       <ExploreGrid />
-    </StyledBox>
+    </PaddedBox>
   );
 }
 
