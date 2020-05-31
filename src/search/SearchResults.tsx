0aa88d64af09b97bfd7e6bcf45daad8dd288a977
mmaramba <mpmaramb@calpoly.edu>
2020-05-31 14:05:33 -0700

    comments

--- src/search/SearchResults.tsx
+++ src/search/SearchResults.tsx
@@ -11,30 +11,35 @@ interface SearchResultProps {
   qry: string;
 }
 
-// Starter file: will be modified for use in graph blocks
+// TODO: union type for GraphMetaData | Story?
 function SearchResults(props: SearchResultProps) {
   const [cards, setCards] = React.useState([] as any);
 
   // TODO: 400 errors? Is there a specific endpoint for public graphs/stories?
   useEffect(() => {
+    console.log(props.hits);
     props.hits.map(item => {
       if (item._index === 'graphs') {
-        getGraphById(item._id).then((data: GraphMetaData) => {
-          console.log(data);
-          setCards((prevArray: any) => [...prevArray, data]);
-        }).catch(err => {
-          console.log("Do something about error");
-        });
+        getGraphById(item._id)
+          .then((data: GraphMetaData) => {
+            console.log(data);
+            setCards((prevArray: any) => [...prevArray, data]);
+          })
+          .catch(err => {
+            console.log('Do something about error');
+          });
       } else {
-        getStoryWithStoryID(item._id).then((data: Story) => {
-          console.log(data);
-          setCards((prevArray: any) => [...prevArray, data]);
-        }).catch(err => {
-          console.log("Do something about error");
-        });
+        getStoryWithStoryID(item._id)
+          .then((data: Story) => {
+            console.log(data);
+            setCards((prevArray: any) => [...prevArray, data]);
+          })
+          .catch(err => {
+            console.log('Do something about error');
+          });
       }
     });
-  }, []);
+  }, [props.hits]);
 
   // TODO: instead of ListItem, use StoryCard/GraphCard
   const makeList = () => {
