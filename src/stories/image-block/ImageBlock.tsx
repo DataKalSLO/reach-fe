56def576fe6d5825b9a28c9f59a4b0ec303f2705
Daniel Heyde <dpheyde@gmail.com>
2020-06-01 15:40:56 -0700

    Added local tracking of uploaded image

--- src/stories/image-block/ImageBlock.tsx
+++ src/stories/image-block/ImageBlock.tsx
@@ -8,11 +8,15 @@ interface Props {
 }
 
 export default function ImageBlock(props: Props) {
-  const { imageUrl, setImageUrl } = props; // this is NOT a hook, the state is being managed in StoryEditor
+  // image url is both stored here and in redux
+  const [localImageUrl, setLocalImageUrl] = useState(props.imageUrl);
+  const setReduxImageUrl = props.setImageUrl;
 
   function logIt(files: [File]) {
     // temporary until backend is connected and we can get actual image url from s3
-    setImageUrl('https://i.redd.it/ni8dp6vf80xy.jpg');
+    const tempUrl = 'https://i.redd.it/ni8dp6vf80xy.jpg';
+    setLocalImageUrl(tempUrl);
+    setReduxImageUrl(tempUrl);
     alert(
       'files uploaded (not really, needs backend)\nEnjoy this placeholder.'
     );
@@ -20,11 +24,11 @@ export default function ImageBlock(props: Props) {
 
   return (
     <StoryBlockContainer variant="outlined">
-      {imageUrl === '' ? (
+      {localImageUrl === '' ? (
         <ImageDropzone onFileDrop={logIt}></ImageDropzone>
       ) : (
         //todo add delete 'x' on top corner of preview
-        <img src={imageUrl} alt="Preview" />
+        <img src={localImageUrl} alt="Preview" />
       )}
     </StoryBlockContainer>
   );
