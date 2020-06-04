2f7e45445789634e0437268ecef50a3ee060bd15
mmaramba <mpmaramb@calpoly.edu>
2020-06-04 11:04:35 -0700

    icons with text for sharemenu

--- src/share/ShareMenu.tsx
+++ src/share/ShareMenu.tsx
@@ -1,3 +1,6 @@
+import { Box, Divider } from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
 import { Facebook, LinkedIn, Twitter } from '@material-ui/icons';
 import React from 'react';
 import {
@@ -5,10 +8,7 @@ import {
   LinkedinShareButton,
   TwitterShareButton
 } from 'react-share';
-import ListItemButton from '../common/components/ListItemButton';
 import { Menu } from '../reach-ui/core';
-import MenuItem from '@material-ui/core/MenuItem';
-import { Divider } from '@material-ui/core';
 
 interface ShareMenuProps {
   shareURL: string;
@@ -21,7 +21,15 @@ interface ShareMenuProps {
   marginTop?: string; // used to control the vertical offset from the top of the button to the top of the menu
 }
 
+const useStyles = makeStyles({
+  text: {
+    paddingLeft: '20px'
+  }
+});
+
 export default function ShareMenu(props: ShareMenuProps) {
+  const classes = useStyles();
+
   return (
     <Menu
       id="story-status-menu"
@@ -32,7 +40,10 @@ export default function ShareMenu(props: ShareMenuProps) {
     >
       <MenuItem onClick={props.closeMenuCallback}>
         <FacebookShareButton url={props.shareURL}>
-          Share to Facebook
+          <Box display="flex">
+            <Facebook />
+            <span className={classes.text}>Share to Facebook</span>
+          </Box>
         </FacebookShareButton>
       </MenuItem>
       <Divider />
@@ -42,7 +53,10 @@ export default function ShareMenu(props: ShareMenuProps) {
           title={props.title}
           via="REACH"
         >
-          Share to Twitter
+          <Box display="flex">
+            <Twitter />
+            <span className={classes.text}>Share to Twitter</span>
+          </Box>
         </TwitterShareButton>
       </MenuItem>
       <Divider />
@@ -52,7 +66,10 @@ export default function ShareMenu(props: ShareMenuProps) {
           title={props.title}
           summary="REACH"
         >
-          Share to LinkedIn
+          <Box display="flex">
+            <LinkedIn />
+            <span className={classes.text}>Share to LinkedIn</span>
+          </Box>
         </LinkedinShareButton>
       </MenuItem>
     </Menu>
