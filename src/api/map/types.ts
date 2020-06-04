b398dc4747b193eba6cbb753efe70f6d008d3d0e
Alex Tschopp <altschop@calpoly.edu>
2020-06-03 19:52:04 -0700

    got rid of extra comments

--- src/api/map/types.ts
+++ src/api/map/types.ts
@@ -1,13 +1,7 @@
-// import {  } from '../../redux/map/types';
-
 export enum MapActions {
   GET_HEATMAP,
   GET_MARKERS
 }
-
-// export interface DataColumnsRecord {
-//   [columName: string]: DataValue[];
-// }
 
 export type MapApiPayload = any;
 export type MapApiResponse = any;
