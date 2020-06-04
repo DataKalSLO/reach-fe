d35367d602327d9e6f598e639131d169fefacb5d
Tanner Larson <tannerlarson07@gmail.com>
2020-06-04 13:01:04 -0700

    connected MyStories on MyStuff page to backend
    only retrieving stories in DRAFT state

--- src/containers/my-stuff-landing/MyStuffStories.tsx
+++ src/containers/my-stuff-landing/MyStuffStories.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect } from 'react';
+import { getStoriesInDraft } from '../../api/stories/operations';
 import MyStuffWrapper from '../../my-stuff/MyStuffWrapper';
-import { getPublishedStoriesAndHandleResponse } from '../../api/stories/operationHandlers';
-import { Story } from '../../redux/story/types';
-import { Gallery } from '../../reach-ui/core';
 import StoryCard from '../../preview-cards/story-card/StoryCard';
-
-const [stories, setStories] = React.useState([] as Story[]);
-
-useEffect(() => {
-  getPublishedStoriesAndHandleResponse().then(apiResponse =>
-    apiResponse ? setStories(apiResponse) : null
-  );
-}, []);
-
-const makeStoryCards = () => {
-  return stories.map((storyInfo: Story) => {
-    return <StoryCard key={storyInfo.id} story={storyInfo} />;
-  });
-};
+import { Story } from '../../redux/story/types';
 
 export default function MyStuffStories() {
+  const [stories, setStories] = React.useState([] as Story[]);
+
+  useEffect(() => {
+    // TODO: need an API call to retrieve all stories by userId
+    // (i.e. stories in REVIEW, PUBLISHED, DRAFT and FEEDBACK)
+    getStoriesInDraft().then(storyData => {
+      console.log(storyData);
+      setStories(storyData);
+    });
+  }, []);
+
+  const makeStoryCards = () => {
+    return stories.map((storyInfo: Story) => {
+      return <StoryCard key={storyInfo.id} story={storyInfo} />;
+    });
+  };
+
   return <MyStuffWrapper title="My Stories">{makeStoryCards()}</MyStuffWrapper>;
 }
