c698acc596104c2e7e71b6cd878e7ecb05e5d949
Kevin Krein <kkrein@calpoly.edu>
2020-06-04 10:47:18 -0700

    Changes to charts & stories

--- src/containers/my-stuff-landing/MyStuffCharts.tsx
+++ src/containers/my-stuff-landing/MyStuffCharts.tsx
@@ -1,55 +1,25 @@
-import React from 'react';
 import MyStuffWrapper from '../../my-stuff/MyStuffWrapper';
 import GraphCard from '../../preview-cards/graph-card/GraphCard';
 import { GraphMetaData } from '../../redux/graphs/types';
+import React, { useState, useEffect } from 'react';
+import { isUndefined } from 'util';
+import { getAllGraphsAndHandleResponse } from '../../api/graphs/operationHandlers';
 
-const sampleGraph: GraphMetaData = {
-  graphId: '043707c7-b204-4150-aac8-d76809613da3',
-  userId: 'test@gmail.com',
-  timeStamp: 1591139888,
-  userName: 'test account',
-  graphTitle:
-    'Covid-19 New vs. Total Confirmed Cases in San Luis Obispo County',
-  snapshotUrl:
-    'https://hourglass-graph-snapshots.s3.us-east-2.amazonaws.com/043707c7-b204-4150-aac8-d76809613da3.svg',
-  dataSources: [
-    {
-      datasetName: 'covid_cases_slo',
-      columnNames: ['day'],
-      seriesType: 'X_AXIS'
-    },
-    {
-      datasetName: 'covid_cases_slo',
-      columnNames: ['new_cases', 'total_cases'],
-      seriesType: 'Y_AXIS'
-    }
-  ],
-  graphOptions: {
-    title: 'Covid-19 New vs. Total Confirmed Cases in San Luis Obispo County',
-    yConfig: { title: 'Number of Cases' },
-    seriesConfigs: [
-      {
-        name: 'New Confirmed Cases',
-        color: '#99D6D7',
-        dataLabels: false,
-        seriesType: 'column'
-      },
-      {
-        name: 'Total Confirmed Cases',
-        color: '#F2D17C',
-        dataLabels: false,
-        seriesType: 'spline'
-      }
-    ]
-  }
-};
+const [graphs, setGraphs] = useState([] as GraphMetaData[]);
 
 export default function MyStuffCharts() {
+  useEffect(() => {
+    getAllGraphsAndHandleResponse().then(response => {
+      if (!isUndefined(response)) {
+        setGraphs(response);
+      }
+    });
+  });
   return (
     <MyStuffWrapper title="My Charts">
-      {/* TODO: replace this with the content for the gallery view */}
-      <GraphCard {...sampleGraph} />
-      <React.Fragment />
+      {graphs.map(graph => (
+        <GraphCard key={graph.graphId} {...graph} />
+      ))}
     </MyStuffWrapper>
   );
 }
--- src/containers/my-stuff-landing/MyStuffStories.tsx
+++ src/containers/my-stuff-landing/MyStuffStories.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MyStuffWrapper from '../../my-stuff/MyStuffWrapper';
+import { getPublishedStoriesAndHandleResponse } from '../../api/stories/operationHandlers';
+import { Story } from '../../redux/story/types';
+import { Gallery } from '../../reach-ui/core';
+import StoryCard from '../../preview-cards/story-card/StoryCard';
+
+const [stories, setStories] = React.useState([] as Story[]);
+
+useEffect(() => {
+  getPublishedStoriesAndHandleResponse().then(apiResponse =>
+    apiResponse ? setStories(apiResponse) : null
+  );
+}, []);
+
+const makeStoryCards = () => {
+  return stories.map((storyInfo: Story) => {
+    return <StoryCard key={storyInfo.id} story={storyInfo} />;
+  });
+};
 
 export default function MyStuffStories() {
-  return (
-    <MyStuffWrapper title="My Stories">
-      {/* TODO: replace this with the content for the gallery view */}
-      <React.Fragment />
-    </MyStuffWrapper>
-  );
+  return <MyStuffWrapper title="My Stories">{makeStoryCards()}</MyStuffWrapper>;
 }
