5a163c22c3adf02ad7b885447f42745c4ede2e7e
Tanner Larson <tannerlarson07@gmail.com>
2020-06-04 12:14:54 -0700

    properly connect MyGraphs to the MyStuff page

--- src/containers/my-stuff-landing/MyStuffCharts.tsx
+++ src/containers/my-stuff-landing/MyStuffCharts.tsx
@@ -1,20 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { isUndefined } from 'util';
+import { getAllGraphsAndHandleResponse } from '../../api/graphs/operationHandlers';
 import MyStuffWrapper from '../../my-stuff/MyStuffWrapper';
 import GraphCard from '../../preview-cards/graph-card/GraphCard';
 import { GraphMetaData } from '../../redux/graphs/types';
-import React, { useState, useEffect } from 'react';
-import { isUndefined } from 'util';
-import { getAllGraphsAndHandleResponse } from '../../api/graphs/operationHandlers';
-
-const [graphs, setGraphs] = useState([] as GraphMetaData[]);
 
 export default function MyStuffCharts() {
+  const [graphs, setGraphs] = useState([] as GraphMetaData[]);
+
   useEffect(() => {
     getAllGraphsAndHandleResponse().then(response => {
       if (!isUndefined(response)) {
         setGraphs(response);
       }
     });
-  });
+  }, []);
+
   return (
     <MyStuffWrapper title="My Charts">
       {graphs.map(graph => (
